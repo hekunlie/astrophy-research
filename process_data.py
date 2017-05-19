f9d4e9fd7915df68486dee9cb468ca1e64d9e2d9
hekunlie <hekun_lee@sjtu.edu.cn>
2017-05-19 08:56:15 +0800

    new version of classification

--- process_data.py
+++ process_data.py
@@ -6,15 +6,18 @@ Created on Wed Mar 15 14:29:31 2017
 """
 import os
 import numpy
+import time
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
-
+ts =time.clock()
 snr= 'SNR>10'
 g1num = 9
 g2num = 17
 fg1 = numpy.linspace(-0.005, 0.005, g1num)
 fg2 = numpy.linspace(-0.011, 0.011, g2num)
+dfg1 = fg1[1]-fg1[0] #the lenght of the intervel
+dfg2 = fg2[1]-fg2[0]
 paths   = []
 path  = "/run/media/lihekun/KLEE/result/w1/" #where the result data file are placed
 pic_path = '/run/media/lihekun/KLEE/pic/w1/'#where the result figures will be created
@@ -49,25 +52,51 @@ if not exist or comm==1:
 
     for k in paths:  # put the data into the corresponding list
         data = numpy.loadtxt(k, skiprows=1)
-        for i in range(len(data)):
-            a1 = numpy.abs(fg1 - data[i, 5])
-            a1m = numpy.where(a1 == numpy.min(a1))[0][0]
-            a2 = numpy.abs(fg2 - data[i, 11])
-            a2m = numpy.where(a2 == numpy.min(a2))[0][0]
-            for m in range(3):
-                if data[i, m] != -10.:
-                    g1[m][fg1[a1m]].append(data[i, m])
-                if data[i, m + 6] != -10.:
-                    g2[m][fg2[a2m]].append(data[i, m + 6])
-            g1[3][fg1[a1m]].append(data[i, 3])
-            fn1[fg1[a1m]].append(data[i, 4])
-            g2[3][fg2[a2m]].append(data[i, 9])
-            fn2[fg2[a2m]].append(data[i, 10])
+        tag1 = data[:,5]
+        tag1.shape = (len(tag1),1)
+        tag2 = data[:,11]
+        tag2.shape = (len(tag2),1)
+        for i in range(g1num):
+            idx1 = tag1 < fg1[i] + dfg1/2
+            idx2 = tag1 > fg1[i] - dfg1/2
+            for na in range(4):
+                ellip1 = data[:,na]
+                ellip1.shape = (len(ellip1),1)
+                g1[na][fg1[i]].extend(numpy.ndarray.tolist(ellip1[idx1&idx2]))
+                if na ==3:
+                    n1 = data[:,na+1]
+                    n1.shape = (len(n1),1)
+                    fn1[fg1[i]].extend(numpy.ndarray.tolist(n1[idx1&idx2]))
+        for i in range(g2num):
+            idx1 = tag2 < fg2[i] + dfg2/2
+            idx2 = tag2 > fg2[i] - dfg2/2
+            for na in range(4):
+                ellip2 = data[:,na+6]
+                ellip2.shape = (len(ellip2),1)
+                g2[na][fg2[i]].extend(numpy.ndarray.tolist(ellip2[idx1&idx2]))
+                if na==3:
+                    n2 = data[:,na+7]
+                    n2.shape = (len(n2),1)
+                    fn2[fg2[i]].extend(numpy.ndarray.tolist(n2[idx1&idx2]))
+
+        # for i in range(len(data)):
+        #     a1 = numpy.abs(fg1 - data[i, 5])
+        #     a1m = numpy.where(a1 == numpy.min(a1))[0][0]
+        #     a2 = numpy.abs(fg2 - data[i, 11])
+        #     a2m = numpy.where(a2 == numpy.min(a2))[0][0]
+        #     for m in range(3):
+        #         if data[i, m] != -10.:
+        #             g1[m][fg1[a1m]].append(data[i, m])
+        #         if data[i, m + 6] != -10.:
+        #             g2[m][fg2[a2m]].append(data[i, m + 6])
+        #     g1[3][fg1[a1m]].append(data[i, 3])
+        #     fn1[fg1[a1m]].append(data[i, 4])
+        #     g2[3][fg2[a2m]].append(data[i, 9])
+        #     fn2[fg2[a2m]].append(data[i, 10])
 
     res_arr1 = numpy.zeros((12, g1num))    # the first 4 rows are the ellipticity,
     res_arr2 = numpy.zeros((12, g2num))    # the second 4 rows are the correspongding error bar,
                                             # the third 4 rows are the correspongding number of samples.
-
     for i in range(4):
         for m in range(len(fg1)):
             if i != 3:
@@ -114,9 +143,9 @@ else:
     text = numpy.loadtxt(path+'cache.dat')
     res_arr1 = text[:,0:g1num]
     res_arr2 = text[:,g1num:]
-
+tm =time.clock()
 # fit the line
-start1=0
+start1=1
 end1 =0
 start2=0
 end2=0
@@ -227,8 +256,9 @@ for i in range(3,4):
     nm2 = pic_path + name[i] + "_g2.png"
     plt.savefig(nm2)
     print('plotted g2')
+te = time.clock()
 print ("Complete")
-
+print(tm-ts,te-tm)
 
 
                    
