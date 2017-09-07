9082b00df7aa3d8d3c99683b5556781f94ff3318
hekun_lee <hekun_lee@sjtu.edu.cn>
2017-09-08 00:02:46 +0800

    to measure new parameters

--- selection_bias_est.py
+++ selection_bias_est.py
@@ -8,16 +8,20 @@ from multiprocessing import Pool
 import pandas
 import galsim
 import time
+import lsstetc
 
 def est_shear(m, g1, g2):
     stamp_size = 80
     chip_num = 100
+    pixel_scale = 0.2
     ahead = '/lmc/selection_bias/%s/'%m
     respath = '/lmc/selection_bias/result/data/'
     if not os.path.isdir(respath):
         os.makedirs(respath)
     psf = fits.open(ahead+'psf.fits')[0].data
-    col = ["KSB_g1", "BJ_e1", "RG_e1", "FQ_G1", "fg1", "KSB_g2", "BJ_e2", "RG_e2", "FQ_G2", "fg2", "FG_N", "FQ_U", "FQ_V", "SNR_ORI"]
+    col = ["KSB_g1", "BJ_e1", "RG_e1", "FQ_G1", "fg1", "KSB_g2", "BJ_e2", "RG_e2", "FQ_G2", "fg2",
+           "FG_N", "FQ_U", "FQ_V",  'kmg1', 'kmg2', 'kme1', 'kme2', 'bme1', 'bme2', 'rme1', 'rme2', "SNR_ORI"]
+    prop = lsstetc.ETC(band='r', pixel_scale=pixel_scale, stamp_size=stamp_size, nvisits=180)
     for k in range(chip_num):
         ts = time.time()
         kk = str(k).zfill(2)
@@ -34,21 +38,32 @@ def est_shear(m, g1, g2):
             idx = kk+'_%s'%gg
             gal_index.append(idx)
             gal = gal_pool[j]
-            noise = numpy.random.normal(loc=0., scale=380.8645, size=stamp_size**2).reshape(stamp_size, stamp_size)
+            noise = numpy.random.normal(loc=0., scale=prop.sigma_sky, size=stamp_size**2).reshape(stamp_size, stamp_size)
             gal_g = galsim.Image(gal)
             psf_g = galsim.Image(psf)
+
             res_k = galsim.hsm.EstimateShear(gal_g, psf_g, shear_est='KSB', strict=False)
             ksb_g1 = res_k.corrected_g1
             ksb_g2 = res_k.corrected_g2
+            ksb_me1 = res_k.observed_shape.e1
+            ksb_me2 = res_k.observed_shape.e2
+            ksb_mg1 = res_k.observed_shape.g1
+            ksb_mg2 = res_k.observed_shape.g2
+
             res_b = galsim.hsm.EstimateShear(gal_g, psf_g, shear_est='BJ', strict=False)
             bj_e1 = res_b.corrected_e1
             bj_e2 = res_b.corrected_e2
+            bj_me1 = res_b.observed_shape.e1
+            bj_me2 = res_b.observed_shape.e2
+
             res_r = galsim.hsm.EstimateShear(gal_g, psf_g, shear_est='REGAUSS', strict=False)
             re_e1 = res_r.corrected_e1
             re_e2 = res_r.corrected_e2
+            re_me1 = res_r.observed_shape.e1
+            re_me2 = res_r.observed_shape.e2
 
             G1, G2, N, U, V = Fourier_Quad().shear_est(gal, psf, stamp_size, noise, N=True)
-            data_matrix[j, :] = ksb_g1, bj_e1, re_e1, G1, g1, ksb_g2, bj_e2, re_e2, G2, g2, N, U, V, cat_data[j]
+            data_matrix[j, :] = ksb_g1, bj_e1, re_e1, G1, g1, ksb_g2, bj_e2, re_e2, G2, g2, N, U, V, ksb_mg1, ksb_mg2, ksb_me1, ksb_me2, bj_me1, bj_me2, re_me1, re_me2, cat_data[j]
 
             #data_matrix[j,:] = 0, 0, 0, G1, N, g1, 0, 0, 0, G2, N, g2, U, V,cat_data[j]
         df = pandas.DataFrame(data_matrix, index=gal_index, columns=col)
