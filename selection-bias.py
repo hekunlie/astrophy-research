92b97da74d8e97b81932924649bbb418c6cbe37e
hekunlie <hekun_lee@sjtu.edu.cn>
2017-09-10 21:46:23 +0800

    change parameters

--- selection-bias.py
+++ selection-bias.py
@@ -21,10 +21,10 @@ def simulate(g1, g2, ellip1, ellip2, gal_radius, gal_radius_s, mag_list, process
     label = range(0, gal_num)
     prop = lsstetc.ETC(band='r', pixel_scale=pixel_scale, stamp_size=stamp_size, nvisits=180)
 
-    psf_o = galsim.Gaussian(half_light_radius=0.7, flux=1.0)
+    psf_o = galsim.Gaussian(half_light_radius=0.8, flux=1.0)
     psf = psf_o.shear(e1=0.081, e2=-0.066)
     psf_noise = numpy.random.normal(loc=0, scale=prop.sigma_sky, size=stamp_size**2).reshape(stamp_size, stamp_size)
-    psf_img = psf.withFlux(prop.flux(21)).drawImage(nx=stamp_size, ny=stamp_size, scale=pixel_scale).array
+    psf_img = psf.withFlux(prop.flux(20.5)).drawImage(nx=stamp_size, ny=stamp_size, scale=pixel_scale).array
     psf_img = psf_img + psf_noise
 
     ahead = '/lmc/selection_bias/%d/' %process_id
@@ -35,6 +35,9 @@ def simulate(g1, g2, ellip1, ellip2, gal_radius, gal_radius_s, mag_list, process
     hdu = fits.PrimaryHDU(psf_img)
     hdu.writeto(psf_path, overwrite=True)
 
+    sersic_rb = gal_radius_s['arr_0']
+    sersic_rd = gal_radius_s['arr_1']
+    rs_tag = 0
     for k in range(chip_num):
         kk = str(k).zfill(2)
         ts = time.time()
@@ -50,7 +53,6 @@ def simulate(g1, g2, ellip1, ellip2, gal_radius, gal_radius_s, mag_list, process
         ell1 = ellip1[tag]
         ell2 = ellip2[tag]
         radius = gal_radius[tag]
-        rs_tag = 0
         for i in range(gal_num):
             e1 = ell1[i]
             e2 = ell2[i]
@@ -58,17 +60,13 @@ def simulate(g1, g2, ellip1, ellip2, gal_radius, gal_radius_s, mag_list, process
             gal_flux = prop.flux(mag)
             morpho = numpy.random.randint(1, 4, 1)[0]
             ra = radius[i]
-            rs = gal_radius_s[rs_tag]
-
             if morpho == 1:
                 gal = galsim.Exponential(flux=gal_flux, half_light_radius=ra)
 
             elif morpho == 2:
+                rb = sersic_rb[tag]
+                rd = sersic_rd[tag]
                 rs_tag += 1
-                if ra >= 0.55:
-                    ra = numpy.random.uniform(0.3, 0.55, 1)[0]
-                rb = ra
-                rd = ra + rs
                 bulge = galsim.Sersic(half_light_radius=rb, n=3.5)
                 disk = galsim.Sersic(half_light_radius=rd, n=1.5)
                 gal = bulge*0.3 + disk*0.7
@@ -99,8 +97,8 @@ if __name__=='__main__':
     shear1 = arr['arr_0']
     shear2 = arr['arr_1']
     mags = numpy.load('/home/hklee/work/selection_bias/parameters/lsstmagsims.npz')['arr_0']
-    gal_ra = numpy.load('/home/hklee/work/selection_bias/parameters/gal_radius.npz')['arr_0']
-    sersic_rd = numpy.load('/home/hklee/work/selection_bias/parameters/sersic_rd.npz')['arr_0']
+    gal_rad = numpy.load('/home/hklee/work/selection_bias/parameters/gal_radius.npz')['arr_0']
+    sersic_rad = numpy.load('/home/hklee/work/selection_bias/parameters/sersic_rd.npz')
     e1e2 = numpy.load('/home/hklee/work/selection_bias/parameters/e1e2.npz')
     ie1 = e1e2['arr_0']
     ie2 = e1e2['arr_1']
@@ -109,7 +107,7 @@ if __name__=='__main__':
     for m in range(len(shear1)):
        ig1 = shear1[m]
        ig2 = shear2[m]
-       p.apply_async(simulate, args=(ig1, ig2, ie1, ie2, gal_ra, sersic_rd, mags, m,))
+       p.apply_async(simulate, args=(ig1, ig2, ie1, ie2, gal_rad, sersic_rad, mags, m,))
     p.close()
     p.join()
     #simulate(shear1[0], shear2[0], ie1, ie2, gal_ra, sersic_rd, mags, 0)
