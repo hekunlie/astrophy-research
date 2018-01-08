4336591cd7f9cbe9543e7b475ff2f3945837e4d2
hekunlie <hekun_lee@sjtu.edu.cn>
2018-01-08 10:36:36 +0800

    write data to hdf5

--- simulation.cpp
+++ simulation.cpp
@@ -30,12 +30,13 @@ int main(int argc, char*argv[])
 
 		/* 10 (g1,g2) points and each pairs contain 100 chips which cotians 10000 gals */
 		int chip_num = 1000, stamp_num = 10000, shear_pairs = 10;
-
-		int i, j, seed;
+		/* remember to change the data_cols when you change the number of estimators recorded */
+		int i, j, seed, data_rows, data_cols=27;
 		int size = 56, num_p = 45, stamp_nx = 100,  psf_type = 2;
 		double psf_scale = 4., max_radius = 9., flux_m, ry, rs, rd, st, ed, s1, s2;
 		double g1=0., g2=0.;
 		double gal_noise_sig = 380.64, psf_noise_sig = 0., thres = 2.72;
+		data_rows = chip_num*stamp_num;
 		rd = 1. / psf_scale / psf_scale;
 		all_paras.gal_noise_sig = gal_noise_sig;
 		all_paras.psf_noise_sig = psf_noise_sig;
@@ -48,7 +49,15 @@ int main(int argc, char*argv[])
 		double *ppow = new double[size*size]();
 		double *noise = new double[size*size]();
 		double *pnoise = new double[size*size]();
-		char chip_path[200], data_path[200], buffer[300], buffer1[200] ;
+		char chip_path[200], data_path[200], buffer[300], buffer1[200], h5_path[100], set_name[20];
+
+		/* the data matrix data[i][j] */
+		double *data_m = new double[data_rows*data_cols]();
+		double **data = new double*[data_rows];
+		for (i = 0; i < data_rows; i++)
+		{
+			data[i] = data_m + i*data_cols;
+		}
 
 		// initialize gsl
 		//seed = myid * 84324 + 46331;
@@ -101,6 +110,9 @@ int main(int argc, char*argv[])
 		sprintf(buffer1, "myid %d  g1: %4f  g2: %4f \n", myid, g1, g2);
 		cout << buffer1;
 
+		sprintf(h5_path, "/lmc/selection_bias/result/data/data_%d.hdf5", myid);
+		sprintf(set_name, "/data");
+
 		for (i = 0; i < chip_num;  i++)
 		{	
 			s1 = clock();
@@ -136,9 +148,25 @@ int main(int argc, char*argv[])
 				initialize(point, num_p * 2);
 				initialize(noise, size*size);
 				initialize(pnoise, size*size);
-				sprintf(buffer, "%g %g %g %.6f %.6f %g %g %g %.6f %.6f %.6f %.6f %.6f %g %g %g %d %.3f %.3f %.3f %.3f %.3f %.6f %.6f %d %d %d \n", 0., 0., 0., all_paras.n1, g1, 0.,0.,0., all_paras.n2, g2, all_paras.dn, all_paras.du, all_paras.dv,
-				0., 0., 0., all_paras.gal_size, all_paras.gal_flux , all_paras.gal_peak , all_paras.gal_snr, all_paras.gal_fsnr, all_paras.gal_osnr ,all_paras.dp1, all_paras.dp2, myid, i, j);
-				fout << buffer;
+
+				data[i*stamp_num + j][3] = all_paras.n1;
+				data[i*stamp_num + j][4] = g1;
+				data[i*stamp_num + j][8] = all_paras.n2;
+				data[i*stamp_num + j][9] = g2;
+				data[i*stamp_num + j][10] = all_paras.dn;
+				data[i*stamp_num + j][11] = all_paras.du;
+				data[i*stamp_num + j][12] = all_paras.dv;
+				data[i*stamp_num + j][16] = (double)(all_paras.gal_size);
+				data[i*stamp_num + j][17] = all_paras.gal_flux;
+				data[i*stamp_num + j][18] = all_paras.gal_peak;
+				data[i*stamp_num + j][19] = all_paras.gal_snr;
+				data[i*stamp_num + j][20] = all_paras.gal_fsnr;
+				data[i*stamp_num + j][21] = all_paras.gal_osnr;
+				data[i*stamp_num + j][22] = all_paras.dp1;
+				data[i*stamp_num + j][23] = all_paras.dp2;
+				data[i*stamp_num + j][24] = (double)(myid);
+				data[i*stamp_num + j][25] = (double)(i);
+				data[i*stamp_num + j][26] = (double)(j);
 			}
 
 			write_img(big_img, stamp_nx*size, stamp_nx*size, chip_path);		
@@ -150,7 +178,7 @@ int main(int argc, char*argv[])
 			sprintf(buffer1, "myid %d: chip %d done in %g \n", myid, i, (s2 - s1) / CLOCKS_PER_SEC);
 			cout << buffer1;
 		}
-		
+		write_h5(h5_path, set_name, data_rows, data_cols, data[0]);
 		ed = clock();
 
 		sprintf(buffer1, "myid %d:  done in %g \n", myid, i, (ed - st) / CLOCKS_PER_SEC);
@@ -166,6 +194,8 @@ int main(int argc, char*argv[])
 		delete[] ppow;
 		delete[] noise;		
 		delete[] pnoise;
+		delete[] data_m;
+		delete[] data;
 		gsl_rng_free();				
 		MPI_Finalize();
 		return 0;
