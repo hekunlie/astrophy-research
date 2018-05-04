c37e6efc3f7c47574a15817a75247f13fea46815
hekunlie <hekun_lee@sjtu.edu.cn>
2018-05-04 10:43:22 +0800

    specify the process_stage in the cmd line

--- fresh_run.py
+++ fresh_run.py
@@ -2,6 +2,7 @@ from subprocess import Popen
 import time
 import os
 import shutil
+from sys import argv
 
 
 # specify the number of cores used
@@ -15,15 +16,23 @@ data_path = "/mnt/ddnfs/data_users/hkli/CFHT/w1234/original/"
 source_list = data_path + "source.list"
 
 # run the three processes
-# one can specify the PROCESS_stage like [1,3]...
-PROCESS_stage = [1, 2, 3]
+# one can specify the PROCESS_stage like [1,3], which comes from the commands from command inputs
+PROCESS_stage = []
+for i in range(len(argv)):
+    if 0 < i < 4:
+        c = int(float(argv[i]))
+        if c in [1, 2, 3]:
+            PROCESS_stage.append(c)
+        else:
+            print("Wrong PROCESS_stage input: %s which should be one of [1, 2, 3]"%argv[i])
+            exit()
 
 # read the para.inc
 f = open(para, "r")
 contents = f.readlines()
 f.close()
 
-dirs = os.listdir(data_path)
+files = os.listdir(data_path)
 initial_target = ["/astrometry", "/stamps", "/result"]
 
 t = [0, 0, 0]
@@ -33,9 +42,9 @@ for i in PROCESS_stage:
 
     # initialize the directories, "../astrometry" or " ../result" or "../stamps" (depending on the PROCESS_stage)
     # it will take a while...
-    for dir in dirs:
+    for file in files:
         if "w" in dir:
-            initial_path = data_path + dir + initial_target[i-1]
+            initial_path = data_path + file + initial_target[i-1]
             print("Deleting the %s"%initial_path)
             shutil.rmtree(initial_path, ignore_errors=True)
             os.mkdir(initial_path)
