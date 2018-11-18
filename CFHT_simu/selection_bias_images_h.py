2955f1d1d75532123f84f0e34b89c2709f7b3744
hekun_lie <hekun_lee@sjtu.edu.cn>
2018-11-18 15:15:46 +0800

    new parallel algorithm

---
+++ CFHT_simu/selection_bias_images_h.py
@@ -0,0 +1,121 @@
+import os
+my_home = os.popen("echo $HOME").readlines()[0][:-1]
+from sys import path
+path.append('%s/work/fourier_quad/'%my_home)
+import numpy
+import galsim
+from astropy.io import fits
+from Fourier_Quad import Fourier_Quad
+import time
+from mpi4py import MPI
+import h5py
+import tool_box
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+cpus = comm.Get_size()
+
+ts = time.clock()
+source = "dimmer"
+envs_path = "%s/work/envs/envs.dat"%my_home
+get_contents = [['selection_bias', "%s_path"%source, '1'],['selection_bias', "%s_path_result"%source, '1'],
+                ['selection_bias', "%s_path_para"%source, '1'],['selection_bias', "%s_path_log"%source, '1']]
+path_items = tool_box.config(envs_path,['get','get','get','get'], get_contents)
+total_path, result_path, para_path, log_path = path_items
+
+# total_path = "/mnt/ddnfs/data_users/hkli/selection_bias_real_dimmer_m/PDF/"
+# result_path = "/mnt/ddnfs/data_users/hkli/selection_bias_real_dimmer_m/PDF/result/"
+# para_path = path_items[0]
+# pic_path = "/mnt/ddnfs/data_users/hkli/selection_bias_real_dimmer_m/PDF/pic/"
+# log_path = "/mnt/ddnfs/data_users/hkli/selection_bias_real_dimmer_m/PDF/log/"
+
+logger = tool_box.get_logger(log_path+"%d_logs.dat"%rank)
+
+stamp_size = 100
+stamp_col = 100
+pixel_scale = 0.187
+shear_num = 14
+noise_sig = 65
+total_chips_num = 500
+seed = rank*344 + 1212
+
+ny, nx = stamp_col*stamp_size, stamp_col*stamp_size
+fq = Fourier_Quad(stamp_size, seed)
+
+psf = galsim.Moffat(beta=3.5, fwhm=0.8, flux=1.0, trunc=2)
+#psf = psf_o.shear(e1=0.081, e2=-0.066)
+
+if rank == 0:
+    psf_img = galsim.ImageD(stamp_size, stamp_size)
+    psf.drawImage(image=psf_img, scale=pixel_scale)
+    hdu = fits.PrimaryHDU(psf_img.array)
+    psf_path = total_path + 'psf.fits'
+    hdu.writeto(psf_path, overwrite=True)
+    logger.info("size: %d, pixel_scale: %.3f, noise_sig: %.2f, total galaxy number: %d"%(stamp_size, pixel_scale,
+                                                                                         noise_sig, total_chips_num))
+logger.info("seed: %d"%seed)
+
+chip_tags = [i for i in range(total_chips_num)]
+chip_tags_rank = tool_box.allot(chip_tags, cpus)[rank]
+
+for shear_id in range(shear_num):
+    shear_cata = para_path + "shear.npz"
+    shear = numpy.load(shear_cata)
+    g1 = shear["arr_0"][shear_id]
+    g2 = shear["arr_1"][shear_id]
+
+    paras = para_path + "para_%d.hdf5" % shear_id
+    f = h5py.File(paras, 'r')
+    e1s = f["/e1"].value
+    e2s = f["/e2"].value
+    radius = f["/radius"].value
+    flux = f["/flux"].value
+    fbt = f['/btr'].value
+    gal_profile = f["/type"].value
+    f.close()
+
+    rng = numpy.random.RandomState(seed + shear_id)
+    t = 0
+    for chip_tag in chip_tags_rank:
+        t1 = time.clock()
+        chip_path = total_path + "%s/gal_chip_%04d.fits"%(shear_id, chip_tag)
+        gal_pool = []
+        logger.info("Start the %04d's chip at shear %02d..."%(chip_tag, shear_id))
+
+        para_n = chip_tag*10000
+
+        for k in range(10000):
+            e1 = e1s[para_n+k, 0]
+            e2 = e2s[para_n+k, 0]
+            gal_flux = flux[para_n+k, 0]
+            ra = radius[para_n+k, 0]
+            btr = fbt[para_n+k, 0]
+            c_profile = gal_profile[para_n+k, 0]
+
+            if c_profile == 1:
+                gal = galsim.DeVaucouleurs(half_light_radius=ra, trunc=4.5*ra, flux_untruncated=True).shear(e1=e1, e2=e2)
+            else:
+                bulge = galsim.Sersic(half_light_radius=ra, n=4, trunc=4.5*ra, flux_untruncated=True)# be careful
+                disk = galsim.Sersic(scale_radius=ra, n=1, trunc=4.5*ra, flux_untruncated=True)# be careful
+                gal = bulge * btr + disk * (1-btr)
+                gal = gal.shear(e1=e1, e2=e2)
+
+            gal_s = gal.withFlux(gal_flux)
+            gal_g = gal_s.shear(g1=g1, g2=g2)
+            gal_c = galsim.Convolve([gal_g, psf])
+
+            img = galsim.ImageD(stamp_size, stamp_size)
+            gal_c.drawImage(image=img, scale=pixel_scale)
+            gal_pool.append(img.array)
+            t += 1
+
+        noise_img = rng.normal(0, noise_sig, nx*ny).reshape((ny, nx))
+        big_chip = fq.stack(gal_pool, stamp_col) + noise_img
+        big_chip = numpy.float32(big_chip)
+        hdu = fits.PrimaryHDU(big_chip)
+        hdu.writeto(chip_path, overwrite=True)
+        t2 = time.clock()
+        logger.info("Finish the %04d's chip at shear %02d in %.2f sec"%(chip_tag, shear_id, t2-t1))
+
+te = time.clock()
+logger.info("Used %.2f sec. %s"%(te-ts, total_path))
