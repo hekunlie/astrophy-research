9666e8d9c826f6fd73b494de822eafd1a189bf44
hekunlie <hekun_lee@sjtu.edu.cn>
2018-11-23 19:37:36 +0800

    the memory-shared version

--- CFHT_simu/selection_bias_images_h.py
+++ CFHT_simu/selection_bias_images_h.py
@@ -1,9 +1,9 @@
-import matplotlib
-matplotlib.use("Agg")
 import os
+
 my_home = os.popen("echo $HOME").readlines()[0][:-1]
-from sys import path,argv
-path.append('%s/work/fourier_quad/'%my_home)
+from sys import path, argv
+
+path.append('%s/work/fourier_quad/' % my_home)
 import numpy
 import galsim
 from astropy.io import fits
@@ -14,35 +14,35 @@ import h5py
 import tool_box
 import matplotlib.pyplot as plt
 
-
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 cpus = comm.Get_size()
 
 ts = time.clock()
-source = argv[1]
-envs_path = "%s/work/envs/envs.dat"%my_home
-get_contents = [['selection_bias', "%s_path"%source, '1'],['selection_bias', "%s_path_result"%source, '1'],
-                ['selection_bias', "%s_path_para"%source, '1'],['selection_bias', "%s_path_log"%source, '1']]
-path_items = tool_box.config(envs_path,['get','get','get','get'], get_contents)
+source, show_t = argv[1], int(argv[2])
+envs_path = "%s/work/envs/envs.dat" % my_home
+get_contents = [['selection_bias', "%s_path" % source, '1'], ['selection_bias', "%s_path_result" % source, '1'],
+                ['selection_bias', "%s_path_para" % source, '1'], ['selection_bias', "%s_path_log" % source, '1']]
+path_items = tool_box.config(envs_path, ['get', 'get', 'get', 'get'], get_contents)
 total_path, result_path, para_path, log_path = path_items
 
-logger = tool_box.get_logger(log_path+"%d_logs.dat"%rank)
+logger = tool_box.get_logger(log_path + "%d_logs.dat" % rank)
 
 stamp_size = 100
 stamp_col = 100
+stamp_num = 10000
 pixel_scale = 0.187
-shear_num = 10
-noise_sig = 65
-total_chips_num = 20
-seed = rank*344 + 1212
+shear_num = 14
+noise_sig = 55
+total_chips_num = 500
+total_gal_num = total_chips_num * stamp_num
+seed = rank * 344 + 1212
 
-ny, nx = stamp_col*stamp_size, stamp_col*stamp_size
+ny, nx = stamp_col * stamp_size, stamp_col * stamp_size
 fq = Fourier_Quad(stamp_size, seed)
 
-psf = galsim.Moffat(beta=3.5, fwhm=1, flux=1.0, trunc=2)
-#psf = psf_o.shear(e1=0.081, e2=-0.066)
-
+# PSF
+psf = galsim.Moffat(beta=3.5, fwhm=0.8, flux=1.0, trunc=2)
 if rank == 0:
     psf_img = galsim.ImageD(stamp_size, stamp_size)
     psf.drawImage(image=psf_img, scale=pixel_scale)
@@ -53,56 +53,94 @@ if rank == 0:
                 %(source,stamp_size, pixel_scale, noise_sig, total_chips_num))
 logger.info("seed: %d"%seed)
 
+# task allocation
 chip_tags = [i for i in range(total_chips_num)]
 chip_tags_rank = tool_box.allot(chip_tags, cpus)[rank]
+
+# allocate the memory blocks for parameters
+d_size = MPI.DOUBLE.Get_size()
+if rank == 0:
+    # bytes for n double elements
+    nbytes = total_gal_num * d_size
+else:
+    nbytes = 0
+
+win_e1s = MPI.Win.Allocate_shared(nbytes, d_size, comm=comm)
+e1s_buf, itemsize = win_e1s.Shared_query(0)
+e1s = numpy.ndarray(buffer=e1s_buf, dtype='d', shape=(total_gal_num, 1))
+
+win_e2s = MPI.Win.Allocate_shared(nbytes, d_size, comm=comm)
+e2s_buf, itemsize = win_e2s.Shared_query(0)
+e2s = numpy.ndarray(buffer=e2s_buf, dtype='d', shape=(total_gal_num, 1))
+
+win_radius = MPI.Win.Allocate_shared(nbytes, d_size, comm=comm)
+radius_buf, itemsize = win_radius.Shared_query(0)
+radius = numpy.ndarray(buffer=radius_buf, dtype='d', shape=(total_gal_num, 1))
+
+win_flux = MPI.Win.Allocate_shared(nbytes, d_size, comm=comm)
+flux_buf, itemsize = win_flux.Shared_query(0)
+flux = numpy.ndarray(buffer=flux_buf, dtype='d', shape=(total_gal_num, 1))
+
+win_fbt = MPI.Win.Allocate_shared(nbytes, d_size, comm=comm)
+fbt_buf, itemsize = win_fbt.Shared_query(0)
+fbt = numpy.ndarray(buffer=fbt_buf, dtype='d', shape=(total_gal_num, 1))
+
+win_gal = MPI.Win.Allocate_shared(nbytes, d_size, comm=comm)
+gal_buf, itemsize = win_gal.Shared_query(0)
+gal_profile = numpy.ndarray(buffer=gal_buf, dtype='d', shape=(total_gal_num, 1))
 
 for shear_id in range(shear_num):
     shear_cata = para_path + "shear.npz"
     shear = numpy.load(shear_cata)
     g1 = shear["arr_0"][shear_id]
     g2 = shear["arr_1"][shear_id]
+
+    # rank 0 read all the parameters which will be shared by other ranks
     if rank == 0:
-        print(g1,g2, type(g1))
-    paras = para_path + "para_%d.hdf5" % shear_id
-    f = h5py.File(paras, 'r')
-    e1s = f["/e1"].value
-    e2s = f["/e2"].value
-    radius = f["/radius"].value
-    flux = f["/flux"].value
-    fbt = f['/btr'].value
-    gal_profile = f["/type"].value
-    f.close()
-    #
-    rng = numpy.random.RandomState(seed + shear_id)
-    # t = 1
-    # fig = plt.figure(figsize=(int(5*len(chip_tags_rank)), 5))
-    for chip_tag in chip_tags_rank:
+        paras = para_path + "para_%d.hdf5" % shear_id
+        f = h5py.File(paras, 'r')
+        e1s[:total_gal_num, 0] = f["/e1"].value[:total_gal_num, 0]
+        e2s[:total_gal_num, 0] = f["/e2"].value[:total_gal_num, 0]
+        radius[:total_gal_num, 0] = f["/radius"].value[:total_gal_num, 0]
+        flux[:total_gal_num, 0] = f["/flux"].value[:total_gal_num, 0]
+        fbt[:total_gal_num, 0] = f['/btr'].value[:total_gal_num, 0]
+        gal_profile[:total_gal_num, 0] = f["/type"].value[:total_gal_num, 0]
+        f.close()
+    comm.Barrier()
+
+    # for checking
+    logger.info("SHEAR ID: %02d, RANk: %02d, e1s: %.3f, e2s: %.3f, radius: %.2f, fbt: %.2f"
+                %(shear_id, rank, e1s[numpy.random.randint(0, total_gal_num, 1)[0]],
+                  e2s[numpy.random.randint(0, total_gal_num, 1)[0]],
+                  radius[numpy.random.randint(0, total_gal_num, 1)[0]],
+                  fbt[numpy.random.randint(0, total_gal_num, 1)[0]]))
+    t = 0
+    for t, chip_tag in enumerate(chip_tags_rank):
         t1 = time.clock()
-        chip_path = total_path + "%s/gal_chip_%04d.fits"%(shear_id, chip_tag)
+
+        rng = numpy.random.RandomState(seed + shear_id + t)
+
+        chip_path = total_path + "%s/gal_chip_%04d.fits" % (shear_id, chip_tag)
         gal_pool = []
-        para_n = chip_tag*10000
-        logger.info("Start the %04d's chip at shear %02d...\n"
-                    "Para from: %d" % (chip_tag, shear_id, para_n))
-        # ax = fig.add_subplot(1, int(len(chip_tags_rank)), t)
-        # t+=1
-        para_no = []
-        prof_no = []
-        for k in range(10000):
-            e1 = e1s[para_n+k, 0]
-            e2 = e2s[para_n+k, 0]
-            gal_flux = flux[para_n+k, 0]
-            ra = radius[para_n+k, 0]
-            btr = fbt[para_n+k, 0]
-            c_profile = gal_profile[para_n+k, 0]
-            para_no.append(para_n+k)
-            prof_no.append(c_profile)
+        logger.info("SHEAR ID: %02d, Start the %04d's chip.." % (shear_id, chip_tag))
+
+        para_n = chip_tag * stamp_num
+
+        for k in range(stamp_num):
+            e1 = e1s[para_n + k, 0]
+            e2 = e2s[para_n + k, 0]
+            gal_flux = flux[para_n + k, 0]
+            ra = radius[para_n + k, 0]
+            btr = fbt[para_n + k, 0]
+            c_profile = gal_profile[para_n + k, 0]
 
             if c_profile == 1:
-                gal = galsim.DeVaucouleurs(half_light_radius=ra, trunc=4.5*ra, flux_untruncated=False).shear(e1=e1, e2=e2)
+                gal = galsim.DeVaucouleurs(half_light_radius=ra, trunc=4.5 * ra, flux_untruncated=True).shear(e1=e1,
+                                                                                                              e2=e2)
             else:
-                bulge = galsim.Sersic(half_light_radius=ra, n=4, trunc=4.5*ra, flux_untruncated=False)# be careful
-                disk = galsim.Sersic(scale_radius=ra, n=1, trunc=4.5*ra, flux_untruncated=False)# be careful
-                gal = bulge * btr + disk * (1-btr)
+                bulge = galsim.Sersic(half_light_radius=ra, n=4, trunc=4.5 * ra, flux_untruncated=True)  # be careful
+                disk = galsim.Sersic(scale_radius=ra, n=1, trunc=4.5 * ra, flux_untruncated=True)  # be careful
+                gal = bulge * btr + disk * (1 - btr)
                 gal = gal.shear(e1=e1, e2=e2)
 
             gal_s = gal.withFlux(gal_flux)
@@ -112,17 +150,15 @@ for shear_id in range(shear_num):
             img = galsim.ImageD(stamp_size, stamp_size)
             gal_c.drawImage(image=img, scale=pixel_scale)
             gal_pool.append(img.array)
+        t += 1
 
-        noise_img = rng.normal(0, noise_sig, nx*ny).reshape((ny, nx))
+        noise_img = rng.normal(0, noise_sig, nx * ny).reshape((ny, nx))
         big_chip = fq.stack(gal_pool, stamp_col) + noise_img
         big_chip = numpy.float32(big_chip)
         hdu = fits.PrimaryHDU(big_chip)
         hdu.writeto(chip_path, overwrite=True)
         t2 = time.clock()
-        logger.info("Finish the %04d's chip at shear %02d in %.2f sec"%(chip_tag, shear_id, t2-t1))
-        # ax.scatter(para_no, prof_no)
-    # pic_path = "/home/hkli/work/test/pic/%d_%d.png"%(shear_id, rank)
-    # plt.savefig(pic_path)
-    # plt.close()
+        logger.info("SHEAR ID: %02d, Finish the %04d's chip in %.2f sec" % (shear_id, chip_tag, t2 - t1))
+
 te = time.clock()
-logger.info("Used %.2f sec. %s"%(te-ts, total_path))
+logger.info("Used %.2f sec. %s" % (te - ts, total_path))
--- samples_of_useful_methods/Python/mpi4py_shared_memory.py
+++ samples_of_useful_methods/Python/mpi4py_shared_memory.py
@@ -1,41 +1,54 @@
 import numpy as np
 from mpi4py import MPI
-
+import h5py
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
+# show how to create two contiguous shared blocks in memory
 # length of double
 itemsize = MPI.DOUBLE.Get_size()
 if rank == 0:
-    # bytes for 10 double elememts
+    # bytes for 10 double elements
     nbytes = 10*itemsize
 else:
     nbytes = 0
 
 # on rank 0 of comm, create the contiguous shared block
-win = MPI.Win.Allocate_shared(nbytes, itemsize, comm=comm)
-
+win1 = MPI.Win.Allocate_shared(nbytes, itemsize, comm=comm)
+win2 = MPI.Win.Allocate_shared(nbytes, itemsize, comm=comm)
 # create a numpy array whose data points to the shared block
 # buf is the block's address in the memory
-buf, itemsize = win.Shared_query(0)
-
+buf1, itemsize = win1.Shared_query(0)
+buf2, itemsize = win2.Shared_query(0)
 
 # create a numpy array from buf
 # code can run successfully without the following step
 # buf = np.array(buf, dtype='float64', copy=False) # may be redundant
 # "d" means double = 'float64'
-ary = np.ndarray(buffer=buf, dtype='d', shape=(10,)) # array filled with zero
-
+ary1 = np.ndarray(buffer=buf1, dtype='d', shape=(10,)) # array filled with zero
+ary2 = np.ndarray(buffer=buf2, dtype='d', shape=(10,1))
 
 # the rank 1 changes the array
 if rank == 1:
-    ary[:5] = np.arange(5, dtype='i')
+    # show how to read data from a hdf5 file and
+    # assign to the created block in memory
+    f = h5py.File("./para.hdf5","r")
+    # ary2 = f["/data"].value does not work,
+    # it must be ary2[:10] = f["/data"].value
+    # or ary2[:10,0] = f["/data"].value[:10,0]
+    # the shapes must match each other
+    ary2[:10] = f["/data"].value
+    f.close()
+
+    # change the array
+    ary1[:5] = np.arange(5, dtype='i')
+
 # wait in process rank 0 of comm until process 1 has written to the array
 comm.Barrier() # necessary
 
 # the you will see that the changed array will be seen by each process
-print(rank, ary)
+print(rank, ary1, ary2)
 
 
 
