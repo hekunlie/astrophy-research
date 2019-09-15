48363b39f2ed1ec42cbb0840411d4bee98ff8104
hekunlie <hekun_lee@sjtu.edu.cn>
2019-09-15 13:59:21 +0800

    Update chisq_plot.py

--- sym_mc_plot/chisq_plot.py
+++ sym_mc_plot/chisq_plot.py
@@ -1,4 +1,3 @@
-
 import os
 my_home = os.popen("echo $MYWORK_DIR").readlines()[0][:-1]
 from sys import path
@@ -8,6 +7,8 @@ import matplotlib.pyplot as plt
 import tool_box
 import h5py
 import matplotlib
+from plot_tool import Image_Plot
+
 
 
 matplotlib.style.use('default')
@@ -29,12 +30,13 @@ def G_bin(G_h, bins, ig_num=0):  # checked 2017-7-9!!!
     num = numpy.histogram(G_h, bins)[0]
     n1 = num[0:int(bin_num / 2)][inverse]
     n2 = num[int(bin_num / 2):]
+    # xi = (n1 - n2) / (n1 + n2)#*numpy.abs(n1-n2)
     xi = (n1 - n2) ** 2 / (n1 + n2)
     delta = n1 - n2
     idx = delta < 0
     xi[idx] = -xi[idx]
     # print(num, n1, n2)
-    return numpy.sum(xi[:len(xi) - ig_num]) * 0.5, xi
+    return numpy.sum(xi[:len(xi) - ig_num]) * 0.5, xi, num
 
 
 def set_bin(data, bin_num, bound_scale):
@@ -53,8 +55,10 @@ sources = [2, 5]
 bin_num = 12
 
 # total_path = "/mnt/ddnfs/data_users/hkli/selection_bias_64_1/"
-total_path = "/mnt/ddnfs/data_users/hkli/simu_test/"
+# total_path = "/mnt/ddnfs/data_users/hkli/selection_bias/paper_data/galsim_dimmer/"
 total_path = "/mnt/ddnfs/data_users/hkli/selection_bias/paper_data/pts_dimmer/"
+
+pic_nm = "chisq_pts.pdf"
 
 shear_cata = total_path + "parameters/shear.npz"
 shear = numpy.load(shear_cata)
@@ -62,20 +66,9 @@ shear = numpy.load(shear_cata)
 fmt = '%2.f%%'
 fig_x = 6
 fig_y = fig_x * 4 / 6
-fonts = 20
-xy_lb_size = 22
-xy_tick_size = xy_lb_size - 5
-legend_size = xy_tick_size
-axis_linewidth = 2.5
-plt_line_width = 2
-cap_size = 5
-tick_len = 6
-figs = (fig_x * 4, fig_y * 2)
-fig = plt.figure(figsize=figs)
-axs = []
-for i in range(8):
-    ax = fig.add_subplot(241 + i)
-    axs.append(ax)
+img = Image_Plot(fig_x=fig_x, fig_y=fig_y)
+img.subplots(2,4)
+
 
 locs = ["upper left", "lower left"]
 legend_loc = [(0.02, 0.97), (0.02, 0.08)]
@@ -95,17 +88,15 @@ for m in range(2):
         idx = 1
     e1 = es_data[:, idx]
 
-    # ch = numpy.random.choice(range(len(e1)), 10000, replace=False)
-
-    # galsim
+    # # galsim
     # para_path = total_path + "parameters/para_%d.hdf5"%source
     # para_h5 = h5py.File(para_path, "r")
     # mag_t = para_h5["/mag"].value
     # e1 = para_h5["/e1"].value
-    # # e1 = para_h5["/%s"%argv[2]].value
     # para_h5.close()
 
     bins = tool_box.set_bin(e1, bin_num, 1.02)
+    num_ini = numpy.histogram(e1, bins)[0]
 
     fourier_path = total_path + "result/data/data_1.5sig/data_%d.hdf5" % source
     f_h5 = h5py.File(fourier_path, "r")
@@ -117,10 +108,9 @@ for m in range(2):
 
     detect_f = f_data[:, -1] > -1
     detect_s = s_data[:, 0] > 0
-    detect_f = detect_s
+
     # P(k=0)
     pk0 = f_data[:, 4]/64/60
-    print(pk0.max(), pk0.min())
 
     # snr_auto
     flux_auto = s_data[:, 1]
@@ -143,30 +133,53 @@ for m in range(2):
     plt_ys = [0, 0]
     plt_xs = [0, 0]
 
-    for i in range(4):
-        ax_tag = m * 4 + i
-        xi_st, xi_t = G_bin(e1, bins)
+    xi_st, xi_t, num_t = G_bin(e1, bins)
+    ellip_scale = (bins[1:] + bins[:bin_num])[int(bin_num/2):]/2
 
-        #         axs[ax_tag].errorbar(range(1, len(xi_t)+1),xi_t, marker="s", label="Total")
-        axs[ax_tag].scatter(range(1, len(xi_t) + 1), xi_t, marker="s", label="Total", facecolor="none", linewidths=2,
-                            color="C0", s=80)
-        axs[ax_tag].plot(range(1, len(xi_t) + 1), xi_t, color="C0")
-        if i == 0 or i == 4:
-            idx = detect_f
+    dscale = (ellip_scale[-1] - ellip_scale[0])*0.05
+    xscale_lim = (ellip_scale[0] - ellip_scale[0]*0.2, ellip_scale[-1] + ellip_scale[-1]*0.1)
+    print(ellip_scale)
+    print(xscale_lim)
+    for i in range(4):
+        # axs[ax_tag].errorbar(range(1, len(xi_t)+1),xi_t, marker="s", label="Total")
+        img.axs[m][i].scatter(range(1, len(xi_t) + 1), xi_t, marker="s", label="Total", facecolor="none",
+                              linewidths=img.plt_line_width, color="C0", s=80)
+        img.axs[m][i].plot(range(1, len(xi_t) + 1), xi_t, color="C0")
+
+        # img.axs[m][i].scatter(ellip_scale, xi_t, marker="s", label="Total", facecolor="none",
+        #                       linewidths=img.plt_line_width, color="C0", s=80)
+        # img.axs[m][i].plot(ellip_scale, xi_t, color="C0")
+
+        # img.axs[m][i].scatter(range(1, len(num_t) + 1), num_t/num_ini, marker="s", label="Total", facecolor="none",
+        #                       linewidths=img.plt_line_width, color="C0", s=80)
+        # img.axs[m][i].plot(range(1, len(num_t) + 1), num_t/num_ini, color="C0")
+
+        if i == 0:
+            idx = detect_s
             # ax.hist(e1[detect_f], bins, alpha=0.8, label="Fourier Quad")
-            xi_s, xi = G_bin(e1[detect_f], bins)
+
+            xi_s, xi, num_s = G_bin(e1[detect_s], bins)
             #             axs[ax_tag].errorbar(range(1, len(xi)+1), xi, marker="s", label="Detected")
-            axs[ax_tag].scatter(range(1, len(xi) + 1), xi, marker="s", label="Detected", facecolor="none", linewidths=2,
-                                color="C1", s=80)
-            axs[ax_tag].plot(range(1, len(xi) + 1), xi, color="C1")
         else:
             idx = detect_s
             # ax.hist(e1[detect_s], bins, alpha=0.8, label="SExtractor")
-            xi_s, xi = G_bin(e1[detect_s], bins)
+
+            xi_s, xi, num_s = G_bin(e1[detect_s], bins)
             #             axs[ax_tag].errorbar(range(1,len(xi)+1), xi,  marker="s",label="Detected")
-            axs[ax_tag].scatter(range(1, len(xi) + 1), xi, marker="s", label="Detected", facecolor="none", linewidths=2,
-                                color="C1", s=80)
-            axs[ax_tag].plot(range(1, len(xi) + 1), xi, color="C1")
+
+        img.axs[m][i].scatter(range(1, len(xi) + 1), xi, marker="s", label="Detected", facecolor="none",
+                              linewidths=img.plt_line_width,   color="C1", s=80)
+        img.axs[m][i].plot(range(1, len(xi) + 1), xi, color="C1")
+
+        # img.axs[m][i].scatter(ellip_scale, xi, marker="s", label="Detected", facecolor="none",
+        #                       linewidths=img.plt_line_width,   color="C1", s=80)
+        # img.axs[m][i].plot(ellip_scale, xi, color="C1")
+        #
+        # img.axs[m][i].scatter(range(1, len(num_s) + 1), num_s/num_ini, marker="s", label="Detected", facecolor="none",
+        #                       linewidths=img.plt_line_width,   color="C1", s=80)
+        # img.axs[m][i].plot(range(1, len(num_s) + 1), num_s/num_ini, color="C1")
+
+        # plot the chi squared of cutoff
         cut_data_sort = numpy.sort(cut_data[i][idx])
         cut_data_step = int(len(cut_data_sort) / cuts_num)
         cutoff_scalar = [cut_data_sort[i * cut_data_step] for i in range(cuts_num)]
@@ -185,13 +198,23 @@ for m in range(2):
                     lb = labels[i] + "$\geq %.2f$" % cut_s
 
             idx_scale = cut_data[i] >= cut_s
-            xi_s, xi = G_bin(e1[idx & idx_scale], bins)
-            #             axs[ax_tag].errorbar(range(1,len(xi)+1), xi,  marker="s", label=lb,facecolor="none",linewidths=2)
-            axs[ax_tag].scatter(range(1, len(xi) + 1), xi, marker="s", label=lb, facecolor="none", linewidths=2,
-                                color="C%d" % (j + 2), s=80)
-            axs[ax_tag].plot(range(1, len(xi) + 1), xi, color="C%d" % (j + 2))
-        ys = axs[ax_tag].set_ylim()
-        xs = axs[ax_tag].set_xlim()
+
+            xi_s, xi, num_s = G_bin(e1[idx & idx_scale], bins)
+            # axs[ax_tag].errorbar(range(1,len(xi)+1), xi,  marker="s", label=lb,facecolor="none",linewidths=img.plt_line_width)
+            img.axs[m][i].scatter(range(1, len(xi) + 1), xi, marker="s", label=lb, facecolor="none",
+                                  linewidths=img.plt_line_width, color="C%d" % (j + 2), s=80)
+            img.axs[m][i].plot(range(1, len(xi) + 1), xi, color="C%d" % (j + 2))
+
+            # img.axs[m][i].scatter(ellip_scale, xi, marker="s", label=lb, facecolor="none",
+            #                       linewidths=img.plt_line_width, color="C%d" % (j + 2), s=80)
+            # img.axs[m][i].plot(ellip_scale, xi, color="C%d" % (j + 2))
+
+            # img.axs[m][i].scatter(range(1, len(num_s) + 1), num_s/num_ini, marker="s", label=lb, facecolor="none",
+            #                       linewidths=img.plt_line_width, color="C%d" % (j + 2), s=80)
+            # img.axs[m][i].plot(range(1, len(num_s) + 1), num_s/num_ini, color="C%d" % (j + 2))
+
+        ys = img.axs[m][i].set_ylim()
+        xs = img.axs[m][i].set_xlim()
         if ys[1] > plt_ys[1]:
             plt_ys[1] = ys[1]
         if ys[0] < plt_ys[0]:
@@ -200,41 +223,44 @@ for m in range(2):
             plt_xs[1] = xs[1]
         if xs[0] < plt_xs[0]:
             plt_xs[0] = xs[0]
-        axs[ax_tag].legend(loc=locs[m], fontsize=legend_size, frameon=False, bbox_to_anchor=legend_loc[m])
+        img.axs[m][i].legend(loc=locs[m], fontsize=img.legend_size, frameon=False, bbox_to_anchor=legend_loc[m])
     # ax.set_ylim(ys[0], ys[1])
     # print(plt_xs, plt_ys)
 
     for i in range(4):
-        ax_tag = m * 4 + i
-        axs[ax_tag].plot([plt_xs[0], plt_xs[1]], [0, 0], linestyle="--", c="grey")
-        axs[ax_tag].set_ylim(plt_ys[0] - 10, plt_ys[1] + 10)
+        # img.axs[m][i].plot([plt_xs[0], plt_xs[1]], [0, 0], linestyle="--", c="grey")
+
+
         if i > 0:
-            axs[ax_tag].set_yticklabels([])
+            img.axs[m][i].set_yticklabels([])
         else:
-            if m == 0:
-                axs[ax_tag].set_yticks(numpy.arange(0, 300, 50))
-            else:
-                axs[ax_tag].set_yticks(numpy.arange(-250, 50, 50))
+            # if m == 0:
+            #     axs[ax_tag].set_yticks(numpy.arange(0, 300, 50))
+            # else:
+            #     axs[ax_tag].set_yticks(numpy.arange(-250, 50, 50))
             if "e1" == cmd:
-                e_label = "$\chi^2_{e_1}$ ($g_1$=%.2f)" % g1
+                e_label = "S$_{e_1}$, $g_1$=%.2f"%g1
             else:
-                e_label = "$\chi^2_{e_2}$ ($g_2$=%.2f)" % g2
-            axs[ax_tag].set_ylabel(e_label, fontsize=xy_lb_size)
+                e_label = "S($e_2$)"
+            img.axs[m][i].set_ylabel(e_label, fontsize=img.xy_lb_size)
+        dy = (plt_ys[1] - plt_ys[0])*0.05
+        img.axs[m][i].set_ylim(plt_ys[0] - dy, plt_ys[1] + dy)
         if m == 1:
-            axs[ax_tag].set_xlabel("Bin label", fontsize=xy_lb_size)
+            text_y = -100
+            img.axs[m][i].set_xlabel("Bin label", fontsize=img.xy_lb_size)
         else:
-            axs[ax_tag].set_xticklabels([])
-        axs[ax_tag].tick_params(direction='in', labelsize=xy_tick_size, top=True, right=True, pad=7)
-        for axis in ["bottom", "left", "top", "right"]:
-            # the line width of the frame
-            axs[ax_tag].spines[axis].set_linewidth(axis_linewidth)
-        axs[ax_tag].xaxis.set_tick_params(which="both", direction="in", length=tick_len, width=axis_linewidth)
-        axs[ax_tag].yaxis.set_tick_params(which="major", direction="in", length=tick_len, width=axis_linewidth)
-        axs[ax_tag].yaxis.set_tick_params(which="minor", direction="in", length=tick_len - 2, width=axis_linewidth)
+            # img.axs[m][i].set_ylim(plt_ys[0], plt_ys[1])
+            text_y = 100
+            img.axs[m][i].set_xticklabels([])
+
+        # img.axs[m][i].text(1.2, text_y, '$g_1$=%.2f'%g1, color='black', ha='left', va='center',  fontsize=img.legend_size)
+
+        # img.axs[m][i].set_xscale("log")
+        # img.axs[m][i].set_xlim(xscale_lim[0], xscale_lim[1])
     print(g1, g2)
-plt.subplots_adjust(wspace=0, hspace=0)
-plt.savefig("chisq_pts.pdf", bbox_inches='tight')
-plt.show()
+img.figure.subplots_adjust(wspace=0, hspace=0)
+img.save_img(pic_nm)
+
 
 
 
