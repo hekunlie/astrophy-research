1d53d5ea8734f0d2ff1dec34befc7bfc7ddf6c89
hekunlie <hekun_lee@sjtu.edu.cn>
2019-11-21 00:04:51 +0800

    for bias_checking

--- selection_bias/CFHT_simu/parameter_check.py
+++ selection_bias/CFHT_simu/parameter_check.py
@@ -13,26 +13,69 @@ from mpi4py import MPI
 import h5py
 import time
 
-sect, source,loops = argv[1], argv[2], int(argv[3])
+total_path, loops = argv[1], int(argv[2])
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 cpus = comm.Get_size()
 
-envs_path = "%s/work/envs/envs.dat"%my_home
-para_path = tool_box.config(envs_path,["get"],[["%s"%sect,"%s_path_para"%source,"0"]])[0]
-
-para_ini_path = para_path+"para.ini"
-paras = tool_box.config(para_ini_path,["get",'get',"get",'get',"get",'get'],
+para_path = total_path + "/parameters"
+para_ini_path = para_path + "/para.ini"
+paras = tool_box.config(para_ini_path,["get",'get',"get",'get',"get",'get','get'],
                         [["para","total_num","0"],["para","stamp_size","0"],
                         ["para", "mag_s", "0"],["para","mag_e","0"],
-                        ["para", "radius_s", "0"],["para","radius_e","0"]])
+                        ["para", "radius_s", "0"],["para","radius_e","0"],
+                         ["para", "shear_num", "0"]])
+
+chip_num = int(paras[0])
+size = int(paras[1])
+num_i = int(chip_num/loops)
+mag_s, mag_e = float(paras[2]), float(paras[3])
+radius_s, radius_e = float(paras[4]), float(paras[5])
+shear_num = int(paras[6])
+stamp_num = 10000
+
+seed_ini = numpy.random.randint(1, 1000000, size=cpus)[rank]
+rng_ini = numpy.random.RandomState(seed_ini)
+seeds = rng_ini.randint(0, 100000, size=2000)
+
+if rank == 0:
+    print(chip_num*stamp_num, size, mag_s, mag_e, radius_s, radius_e)
+comm.Barrier()
+
+# there are many shear points,
+# the threads will process their own points
+m, n = divmod(shear_num, cpus)
+shear_st = m*rank
+shear_ed = m*(rank+1)
+if rank == cpus - 1:
+    shear_ed += n
 
 
-pic = para_path + "/pic/ellip_%d.png"%rank
-plt.figure(figsize=(16,16))
+for shear_id in range(shear_st, shear_ed):
+    h5_path = para_path+'/para_%d.hdf5'%shear_id
 
-h5_path = para_path+'para_%d.hdf5'%rank
-f = h5py.File(h5_path,"r")
+    f = h5py.File(h5_path,"w")
+
+    # magnitude & flux
+    flux, mag = numpy.zeros((chip_num*stamp_num, 1)),numpy.zeros((chip_num*stamp_num, 1))
+    for i in range(loops):
+        time.sleep(rank * 0.05)
+        mag_i = tool_box.mag_generator(num_i*stamp_num, mag_s, mag_e)
+        flux_i = tool_box.mag_to_flux(mag_i)
+        sp, ep = i*num_i*stamp_num, (i+1)*num_i*stamp_num
+        mag[sp: ep, 0] = mag_i
+        flux[sp: ep, 0] = flux_i
+
+    plt.figure(figsize=(8,6))
+    f["/flux"] = flux
+    f["/mag"] = mag
+    f.close()
+
+    pic = para_path + "/pic/mag_%d.png"%shear_id
+    plt.title("MAG")
+    plt.hist(mag, 100)
+    plt.savefig(pic)
 
 
+comm.Barrier()
---
+++ selection_bias/CFHT_simu/shear_points.py
@@ -0,0 +1,32 @@
+import numpy
+from sys import argv
+import h5py
+import matplotlib.pyplot as plt
+
+
+# data_path, shear_num = argv[1], int(argv[2])
+
+shear_num = 32
+data_path = "D:/"
+
+g1 = numpy.linspace(-0.04, 0.04, shear_num)
+g2 = numpy.linspace(-0.04, 0.04, shear_num)
+print(g1)
+numpy.random.shuffle(g1)
+numpy.random.shuffle(g2)
+g = numpy.zeros((2*shear_num,))
+g[:shear_num] = g1
+g[shear_num:] = g2
+plt.subplot(131)
+plt.scatter(g1,g2)
+plt.subplot(132)
+plt.scatter(g1,g1)
+plt.subplot(133)
+plt.scatter(g2,g2)
+plt.show()
+h5f = h5py.File(data_path+"shear.hdf5","w")
+h5f["/g1"] = g1
+h5f["/g2"] = g2
+h5f.close()
+
+numpy.savetxt(data_path+"shear.dat",g)
