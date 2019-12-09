e21de16e9b9d803b7e24861bba09cf97e9dc19dd
hekunlie <hekun_lee@sjtu.edu.cn>
2019-12-09 23:31:51 +0800

    Update cutoff_new.cpp

--- selection_bias/sym_mc_plot/cutoff_new.cpp
+++ selection_bias/sym_mc_plot/cutoff_new.cpp
@@ -1,6 +1,7 @@
 #include<FQlib.h>
 #include<hk_iolib.h>
 #include<hk_mpi.h>
+#define PDF_SYM_RANGE
 
 int main(int argc, char**argv)
 {
@@ -277,12 +278,21 @@ int main(int argc, char**argv)
 
 		try
         {
-			//find_shear(mg1, mnu1, source_count, 8, gh1, gh1_sig, chi_check, chi_fit_num);
-
+#ifdef PDF_SYM_RANGE
+			if(i == 0){std::cout<<"PDF_SYM Range fit"<<std::endl;}
 			find_shear_mean(mg1, mnu1, source_count, gh1, gh1_sig, 100);
 			left = gh1 - 0.025;
 			right = gh1 + 0.025;
 			find_shear_fit(mg1, mnu1, source_count, 8, chi_fit_num, chi_check, left, right, gh1, gh1_sig);
+#endif
+#ifdef PDF_SYM
+			if(i == 0){std::cout<<"PDF_SYM"<<std::endl;}	
+			find_shear(mg1, mnu1, source_count, 8, gh1, gh1_sig, chi_check, chi_fit_num);
+#endif
+#ifdef MEAN	
+			if(i == 0){std::cout<<"MEAN"<<std::endl;}		
+			find_shear_mean(mg1, mnu1, source_count, gh1, gh1_sig, 100);
+#endif
 		}
 		catch(const char *msg)
 		{
@@ -292,12 +302,18 @@ int main(int argc, char**argv)
 		}
 		try
         {
-			//find_shear(mg2, mnu2, source_count, 8, gh2, gh2_sig, chi_check, chi_fit_num);
-
+#ifdef PDF_SYM_RANGE
 			find_shear_mean(mg2, mnu2, source_count, gh2, gh2_sig, 100);
 			left = gh2 - 0.025;
 			right = gh2 + 0.025;
 			find_shear_fit(mg2, mnu2, source_count, 8, chi_fit_num, chi_check, left, right, gh2, gh2_sig);
+#endif
+#ifdef PDF_SYM
+			find_shear(mg2, mnu2, source_count, 8, gh2, gh2_sig, chi_check, chi_fit_num);
+#endif
+#ifdef MEAN
+			find_shear_mean(mg2, mnu2, source_count, gh2, gh2_sig, 100);
+#endif
 		}
 		catch(const char *msg)
 		{
