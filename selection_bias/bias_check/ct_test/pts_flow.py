4143528249a84f282e09267cfc45b5fcd2e681e3
hekunlie <hekun_lee@sjtu.edu.cn>
2020-06-09 09:55:59 +0800

    Update pts_flow.py

--- selection_bias/bias_check/ct_test/pts_flow.py
+++ selection_bias/bias_check/ct_test/pts_flow.py
@@ -1,4 +1,4 @@
-from sys import path
+from sys import path,argv
 path.append("D:/GitHub/astrophy-research/mylib")
 path.append("/home/hklee/work/mylib")
 path.append("/home/hkli/work/mylib")
@@ -16,8 +16,9 @@ numprocs = comm.Get_size()
 data_nm = ["data_gal_noise_cross_term_epsf", "data_noise_free_epsf" ,"data_noise_noise_cross_term_epsf",
            "data_noise_noise_cross_term_est_epsf", "data_noise_residual_epsf","data_noisy_cpp_epsf",
            "data_pure_gal_noise_cross_term_est_epsf"]
+component_tag = int(argv[1])
 
-for shear_tag in range(8):
+for shear_tag in range(1):
 
     h5f = h5py.File("./data/data_noise_free_epsf_%d.hdf5"%shear_tag, "r")
     shear_est_nf = h5f["/data"][()]
@@ -59,6 +60,7 @@ for shear_tag in range(8):
     xylim = 0
     xy_lock = -1
     move_steps = 200
+    show_num = 150
     gh = numpy.linspace(-0.1, 0.1, move_steps)
 
     m,n = divmod(move_steps,numprocs)
@@ -77,9 +79,12 @@ for shear_tag in range(8):
 
                 if i < img_row - 1:
                     tag = i * img_col + j
-                    G1_hat = datas[tag][:150, 0] - gh[k] * (datas[tag][:150, 2] + datas[tag][:150, 3])
-                    img.axs[i][j].scatter(G1_hat,datas[tag][:150,1],
-                                          s=6, label="g1=%.3f"%gh[k])
+                    if component_tag == 0:
+                        G1_hat = datas[tag][:show_num, 0] - gh[k] * (datas[tag][:show_num, 2] + datas[tag][:show_num, 3])
+                        img.axs[i][j].scatter(G1_hat, datas[tag][:show_num, 1], s=6, label="g1=%.3f" % gh[k])
+                    else:
+                        G1_hat = datas[tag][:show_num, 1] - gh[k] * (datas[tag][:show_num, 2] - datas[tag][:show_num, 3])
+                        img.axs[i][j].scatter(G1_hat, datas[tag][:show_num,0], s=6, label="g2=%.3f"%gh[k])
                     img.set_sci_ax(i,j,0)
                     img.set_sci_ax(i,j,1)
 
@@ -93,8 +98,12 @@ for shear_tag in range(8):
                             xylim=xs_
                 else:
                     tag = (i-1) * img_col + j
-                    G1_hat = datas[tag][:, 0] - gh[k] * (datas[tag][:, 2] + datas[tag][:, 3])
-                    img.axs[i][j].hist(G1_hat, 50, label="g1=%.3f"%gh[k])
+                    if component_tag == 0:
+                        G1_hat = datas[tag][:, 0] - gh[k] * (datas[tag][:, 2] + datas[tag][:, 3])
+                        img.axs[i][j].hist(G1_hat, 50, label="g1=%.3f"%gh[k])
+                    else:
+                        G1_hat = datas[tag][:, 1] - gh[k] * (datas[tag][:, 2] - datas[tag][:, 3])
+                        img.axs[i][j].hist(G1_hat, 50, label="g2=%.3f"%gh[k])
                     img.set_sci_ax(i, j, 1)
                     img.set_sci_ax(i, j, 0)
 
@@ -110,7 +119,10 @@ for shear_tag in range(8):
                     img.axs[i][j].plot([-xylim,xylim],[0,0],ls="dotted",c="grey",alpha=0.5)
                     img.axs[i][j].plot([0, 0], [-xylim, xylim], ls="dotted", c="grey", alpha=0.5)
                     img.axs[i][j].set_ylim(-xylim, xylim)
-                    img.set_label(i,j,0,"G2",fontsize=img.xy_lb_size)
+                    if component_tag == 0:
+                        img.set_label(i,j,0,"G2",fontsize=img.xy_lb_size)
+                    else:
+                        img.set_label(i, j, 0, "G1", fontsize=img.xy_lb_size)
                     img.axs[i][j].set_title(titles[tag])
 
                 else:
@@ -121,10 +133,17 @@ for shear_tag in range(8):
                     img.axs[i][j].plot([0, 0], [ys[0], ys[1]], ls="dotted", c="grey", alpha=0.5)
 
                 # img.axs[i][j].set_xlim(-xylim, xylim)
-                img.set_label(i, j, 1, "G1",fontsize=img.xy_lb_size)
+                if component_tag == 0:
+                    img.set_label(i, j, 1, "G1",fontsize=img.xy_lb_size)
+                else:
+                    img.set_label(i, j, 1, "G2",fontsize=img.xy_lb_size)
                 img.axs[i][j].legend()
 
-        img.save_img("./data/pic%d/move_%d.png"%(shear_tag,k))
+        if component_tag == 0:
+            img.save_img("./data/pic%d/G1_move_%d.png"%(shear_tag,k))
+        else:
+            img.save_img("./data/pic%d/G2_move_%d.png"%(shear_tag,k))
+
         # img.show_img()
         img.close_img()
     comm.Barrier()
