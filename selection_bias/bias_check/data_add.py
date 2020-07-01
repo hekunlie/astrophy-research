77774880c0112a6b6c6d89b3e8648bfefb033612
hekunlie <hekun_lee@sjtu.edu.cn>
2020-07-01 08:34:38 +0800

    update

--- selection_bias/bias_check/data_add.py
+++ selection_bias/bias_check/data_add.py
@@ -12,63 +12,87 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 numprocs = comm.Get_size()
 
-data_path, theta_tag = argv[1], int(argv[2])
+data_path, theta_tag, nu_mode = argv[1], int(argv[2]), int(argv[3])
 
-theta = numpy.linspace(0, numpy.pi, 17)[theta_tag]
+# theta = numpy.linspace(0, numpy.pi, 17)[theta_tag]
+#
+# rot_2theta = [[numpy.cos(2*theta), -numpy.sin(2*theta)],
+#               [numpy.sin(2*theta), numpy.cos(2*theta)]]
+#
+# rot_4theta = [[numpy.cos(4*theta), -numpy.sin(4*theta)],
+#               [numpy.sin(4*theta), numpy.cos(4*theta)]]
 
-rot_2theta = [[numpy.cos(2*theta), -numpy.sin(2*theta)],
-              [numpy.sin(2*theta), numpy.cos(2*theta)]]
-
-rot_4theta = [[numpy.cos(4*theta), -numpy.sin(4*theta)],
-              [numpy.sin(4*theta), numpy.cos(4*theta)]]
-
-data_nm_1 = data_path + "/data_noise_free_%d.hdf5"%rank
-data_nm_2 = data_path + "/data_gal_noise_cross_term_%d.hdf5"%rank
-data_nm_3 = data_path + "/data_pure_gal_noise_cross_term_est_%d.hdf5"%rank
-# data_nm_2 = data_path + "/data_noise_free_%d.hdf5"%rank
-
-h5f = h5py.File(data_nm_1, "r")
-mg1 = h5f["/mg1"][()]
-mg2 = h5f["/mg2"][()]
-mn = h5f["/mn"][()]
-mu = h5f["/mu"][()]
-mv = h5f["/mv"][()]
-h5f.close()
-
-h5f = h5py.File(data_nm_2,"r")
-mg1 += h5f["/mg1"][()]
-mg2 += h5f["/mg2"][()]
-mn += h5f["/mn"][()]
-mu += h5f["/mu"][()]
-mv += h5f["/mv"][()]
-h5f.close()
-
-h5f = h5py.File(data_nm_3, "r")
-mg1_ct = h5f["/mg1"][()]
-mg2_ct = h5f["/mg2"][()]
-mn_ct = h5f["/mn"][()]
-mu_ct = h5f["/mu"][()]
-mv_ct = h5f["/mv"][()]
-h5f.close()
-
-mg1 = mg1 + mg1_ct*rot_2theta[0][0] + mg2_ct*rot_2theta[0][1]
-mg2 = mg2 + mg1_ct*rot_2theta[1][0] + mg2_ct*rot_2theta[1][1]
-
-mn = mn + mn_ct
-mu = mu + mu_ct*rot_4theta[0][0] + mv_ct*rot_4theta[0][1]
-mv = mv + mu_ct*rot_4theta[1][0] + mv_ct*rot_4theta[1][1]
+rotation = [[[1,0],[0,1]],
+            [[1,0],[0,-1]],
+            [[-1,0],[0,1]],
+            [[-1,0],[0,-1]],
+            [[0,1],[1,0]],
+            [[0,1],[-1,0]],
+            [[0,-1],[1,0]],
+            [[0,-1],[-1,0]]][theta_tag]
 
 if rank == 0:
-    print(theta)
+    print("%d %d\n%d %d"%(rotation[0][1],rotation[0][1],rotation[1][0],rotation[1][1]))
     if not os.path.exists(data_path + "/mix"):
         os.makedirs(data_path + "/mix")
-
 comm.Barrier()
 
-h5f = h5py.File(data_path + "/mix/data_mix_%d.hdf5"%rank,"w")
-h5f["/mg1"] = numpy.float32(mg1)
-h5f["/mg2"] = numpy.float32(mg2)
-h5f["/mn"] = numpy.float32(mn)
-h5f["/mu"] = numpy.float32(mu)
-h5f["/mv"] = numpy.float32(mv)
-h5f.close()
+my_task = tool_box.alloc([i for i in range(40)], numprocs)[rank]
+
+for i in my_task:
+    data_nm_1 = data_path + "/data_noise_free_%d.hdf5"%i
+    data_nm_2 = data_path + "/data_gal_noise_cross_term_%d.hdf5"%i
+    data_nm_3 = data_path + "/data_pure_gal_noise_cross_term_est_r_%d.hdf5"%i
+    # data_nm_2 = data_path + "/data_noise_free_%d.hdf5"%i
+
+    h5f = h5py.File(data_nm_1, "r")
+    mg1 = h5f["/mg1"][()]
+    mg2 = h5f["/mg2"][()]
+    mn = h5f["/mn"][()]
+    mu = h5f["/mu"][()]
+    mv = h5f["/mv"][()]
+    h5f.close()
+
+    h5f = h5py.File(data_nm_2, "r")
+    mg1 += h5f["/mg1"][()]
+    mg2 += h5f["/mg2"][()]
+    mn += h5f["/mn"][()]
+    mu += h5f["/mu"][()]
+    mv += h5f["/mv"][()]
+    h5f.close()
+
+    h5f = h5py.File(data_nm_3, "r")
+    mg1_ct = h5f["/mg1"][()]
+    mg2_ct = h5f["/mg2"][()]
+    mn_ct = h5f["/mn"][()]
+    mu_ct = h5f["/mu"][()]
+    mv_ct = h5f["/mv"][()]
+    h5f.close()
+
+    mg1 = mg1 + mg1_ct*rotation[0][0] + mg2_ct*rotation[0][1]
+    mg2 = mg2 + mg1_ct*rotation[1][0] + mg2_ct*rotation[1][1]
+
+    if nu_mode == 0:
+        mn = mn + mn_ct
+        mu = mu - mu_ct#*rot_4theta[0][0] + mv_ct*rot_4theta[0][1]
+        mv = mv - mv_ct#*rot_4theta[1][0] + mv_ct*rot_4theta[1][1]
+    elif nu_mode == 1:
+        mu = mu - mu_ct#*rot_4theta[0][0] + mv_ct*rot_4theta[0][1]
+        mv = mv - mv_ct#*rot_4theta[1][0] + mv_ct*rot_4theta[1][1]
+    elif nu_mode == 2:
+        mn = mn + mn_ct
+        mu = mu + mu_ct#*rot_4theta[0][0] + mv_ct*rot_4theta[0][1]
+        mv = mv + mv_ct#*rot_4theta[1][0] + mv_ct*rot_4theta[1][1]
+    else:
+        mu = mu + mu_ct#*rot_4theta[0][0] + mv_ct*rot_4theta[0][1]
+        mv = mv + mv_ct#*rot_4theta[1][0] + mv_ct*rot_4theta[1][1]
+
+    h5f = h5py.File(data_path + "/mix/data_mix_%d.hdf5"%i,"w")
+    h5f["/mg1"] = numpy.float32(mg1)
+    h5f["/mg2"] = numpy.float32(mg2)
+    h5f["/mn"] = numpy.float32(mn)
+    h5f["/mu"] = numpy.float32(mu)
+    h5f["/mv"] = numpy.float32(mv)
+    h5f.close()
+
+comm.Barrier()
--- selection_bias/bias_check/run.py
+++ selection_bias/bias_check/run.py
@@ -1,10 +1,14 @@
 from subprocess import Popen
 
-for i in range(17):
-    cmd = "mpirun -np 40 python data_add.py /mnt/perc/hklee/bias_check/data_from_pi/new_data/data %d"%i
-    a = Popen(cmd, shell=True)
-    a.wait()
+parent_path = "/mnt/perc/hklee/bias_check/data_from_pi/new_data/data"
+for j in range(4):
+    for i in range(8):
+        cmd = "mpirun -np 20 python data_add.py %s %d %d"%(parent_path, i, j)
+        a = Popen(cmd, shell=True)
+        a.wait()
 
-    cmd = "mpirun -np 40 ./calculate /mnt/perc/hklee/bias_check/data_from_pi/new_data/data/mix2 NF_add_CT_add_rpureCTest_rpsf_gg_%d mix 5"%i
-    a = Popen(cmd, shell=True)
-    a.wait()
+        cmd = "mpirun -np 40 ./calculate %s/mix NF_add_CT_add_rpureCTest_rpsf_%d_%d mix 5"%(parent_path,j,i)
+        a = Popen(cmd, shell=True)
+        a.wait()
+
+        print(j,i)
