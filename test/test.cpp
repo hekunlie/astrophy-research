d382fdb597d59c4f6d9ee7797d4109a83ec03ab9
hekunlie <hekun_lee@sjtu.edu.cn>
2020-08-27 22:37:08 +0800

    update test

--- test/test.cpp
+++ test/test.cpp
@@ -3,8 +3,29 @@
 
 int main()
 {   
-    float i,b;
-    b=3;
-    i = std::max(i, b);
+    double temp_x_, temp_y_;
+    int ix_, iy_;
+    double temp_x, temp_y;
+    int ix, iy, bin_num,bin_num2;
+    int i, im, im1, im2;
+    bin_num = 10;
+    bin_num2 = bin_num/2;
+
+    ix_ = 8;
+    iy_ = 4;
+    
+    temp_x = -1;
+    temp_x_ = 0;
+    if(temp_x >= temp_x_)
+    {
+        for(im=ix_; im<bin_num; im++)
+        {std::cout<<im<<" "<<im+1<<std::endl;}
+    }
+    else
+    {
+        for(im=ix_; im>-1; im--)
+        {std::cout<<im<<" "<<im+1<<std::endl;}
+    }
+    std::cin>>i;
     return 0;
 }
