8cbedeaa588064288e3229203deb061a0c856944
hekunlie <hekun_lee@sjtu.edu.cn>
2020-12-28 19:49:57 +0800

    update

--- CFHTLenS/Fourier_Quad/correlation_exposure_wise/pk_tool.py
+++ CFHTLenS/Fourier_Quad/correlation_exposure_wise/pk_tool.py
@@ -1,23 +1,46 @@
-def get_lrange(kmin,kmax, zmin, zmax, H0, Omeg_m0):
-    cosmos = FlatLambdaCDM(H0=H0,Om0=Omeg_m0)
-    dist_min = cosmos.comoving_distance(zmin).value*H0/100
-    dist_max = cosmos.comoving_distance(zmax).value*H0/100
-    lmax = kmax*dist_max
-    lmin = kmin*dist_min
-    print("%.4f < l <%.4f"%(lmin, lmax))
-    return lmin, lmax
+def get_factorial(x):
+    if x == 0:
+        return 1.
+    else:
+        j = 1.
+        for i in range(1, x + 1):
+            j *= i
+        return j
 
-def z2dist(zebin, H0, omega_m0, omega_k=0):
+
+def bessel_zero(x, m):
+    bs = 1.
+    one = 1
+    x2 = x / 2.
+
+    for i in range(1, m):
+        one = -one
+        a = x2 ** (i)
+        b = 1.
+        for j in range(1, i + 1):
+            b *= j
+        bs += one * (a / b) ** 2
+    return bs
+
+
+def z2dist(zpts, H0, omega_m0, omega_k=0):
     cosmos = FlatLambdaCDM(H0, omega_m0)
     h = H0 / 100
 
-    num = zebin.shape[0]
-    dist_bin = numpy.zeros_like(zebin)
+    com_dist = cosmos.comoving_distance(zpts).value * h
+    delta_com_dist = com_dist[1:] - com_dist[:-1]
 
-    for i in range(num):
-        dist_bin[i] = cosmos.comoving_distance(zebin[i]).value * h
+    return com_dist, delta_com_dist
 
-    return dist_bin
+
+def get_lrange(kmin, kmax, zmin, zmax, H0, Omeg_m0):
+    cosmos = FlatLambdaCDM(H0=H0, Om0=Omeg_m0)
+    dist_min = cosmos.comoving_distance(zmax).value * H0 / 100
+    dist_max = cosmos.comoving_distance(zmax).value * H0 / 100
+    lmax = kmax * dist_max
+    lmin = kmin * dist_min
+    print("%.4f < l <%.4f" % (lmin, lmax))
+    return lmin, lmax
 
 
 def get_nz(z, ztomo_bin, ze, ze_bin_num):
@@ -26,15 +49,16 @@ def get_nz(z, ztomo_bin, ze, ze_bin_num):
     z_bin_num = int((z.max() - z.min()) / 0.01) + 2
     z_bin = [z.min() + 0.01 * i for i in range(z_bin_num)]
 
-    ze_bin = numpy.linspace(ze.min(), ze.max(), ze_bin_num + 1)
-
-    dist_bin = z2dist(ze_bin, 70, 0.27)
+    ze_bin = numpy.linspace(0, ze.max() + 0.0001, ze_bin_num + 1)
+    #     # add a zero to the head of ze_bin for the integral later starts from 0.
+    #     ze_bin_cent = numpy.zeros_like(ze_bin)
+    ze_bin_cent = (ze_bin[1:] + ze_bin[:-1]) / 2
 
     zhist = []
     zehist = []
 
-    img = Image_Plot()
-    img.subplots(1, 4)
+    img = Image_Plot(xpad=0.25)
+    img.subplots(1, 2)
 
     for i in range(redshift_bin_num):
         idx1 = z >= ztomo_bin[i]
@@ -53,58 +77,108 @@ def get_nz(z, ztomo_bin, ze, ze_bin_num):
         zehist.append(zz_e_dens)
 
         img.axs[0][0].hist(zz, z_bin, histtype="step", label="[%.2f, %.2f]" % (ztomo_bin[i], ztomo_bin[i + 1]))
-        img.axs[0][1].plot(ze_bin[:-1], zz_e_dens, label="[%.2f, %.2f]" % (ztomo_bin[i], ztomo_bin[i + 1]))
-        img.axs[0][2].plot(dist_bin[:-1], zz_e_dens, label="[%.3f, %.3f]" % (dist_bin[i], dist_bin[i + 1]))
+        img.axs[0][1].plot(ze_bin_cent, zz_e_dens, label="[%.2f, %.2f]" % (ztomo_bin[i], ztomo_bin[i + 1]))
 
-    img.axs[0][3].plot(ze_bin, dist_bin)
     img.show_img()
-    #     print(ze_bin)
-    #     print(dist_bin)
 
-    return zhist, z_bin, zehist, ze_bin
+    return zhist, z_bin, zehist, ze_bin, ze_bin_cent
 
 
-def get_lenq(dist, nz):
-    nz_dist = nz / dist
-
+def get_lenq(com_dist, nz):
     zpts_num = nz.shape[0]
     qx = numpy.zeros((zpts_num,))
+    nz_dist = nz / com_dist
 
-    for i in range(zpts_num - 1):
-        qx[i] = nz[i:].sum() - dist[i] * (nz_dist[i:].sum() - (nz_dist[i] + nz_dist[-1]) / 2)
+    for i in range(zpts_num):
+        qx[i] = nz[i:].sum() + numpy.sum(nz_dist[i:]) * com_dist[i]
 
     return qx
 
 
-def get_pkl(H0, omega_m0, As, zmax, zbin, nz_i, nz_j):
-    c = 299792.458  # Km/s
-    h = H0 / 100  # Km/s/Mpc
-
-    alpha = 9 / 4 * omega_m0 ** 2 * (H0 / c) ** 4  # Mpc^{-4}
-
-    # midle point of each z bin
-    zbin_mid = (zbin[1:] + zbin[:-1]) / 2
-    zpts_num = zbin_mid.shape[0]
-
-    scale_factor_sq_inve = (zbi_mid + 1) ** 2
-
-    dist_bin_mid = z2dist(zbin_mid, H0, omega_m0)
-
-    len_qi = get_lenq(dist_bin_mid, nz_i)
-    len_qj = get_lenq(dist_bin_mid, nz_j)
-
-    omg_b0h2 = 0.05 * (H0 / 100) ** 2
-    omg_m0h2 = omega_m0 * (H0 / 100) ** 2
-
-    # Now get matter power spectra and sigma8 at redshift 0 and 0.8
+def get_CambResult(H0, omg_cm0h2, omg_bm0h2, As, ns, zpts, kpts_num=300, kmin=1e-4, kmax=2):
+    # set parameters
     pars = camb.CAMBparams()
-    pars.set_cosmology(H0=H0, ombh2=omg_b0h2, omch2=omg_m0h2)
-    pars.InitPower.set_params(As=As, ns=0.965)
-    # Note non-linear corrections couples to smaller scales than you want
-    pars.set_matter_power(redshifts=zbin, kmax=3)
+    pars.set_cosmology(H0=H0, ombh2=omg_bm0h2, omch2=omg_cm0h2)
+    pars.InitPower.set_params(As=As, ns=ns)
+    pars.set_matter_power(redshifts=zpts, kmax=kmax)
 
     # Non-Linear spectra (Halofit)
     pars.NonLinear = model.NonLinear_both
+    pars.NonLinearModel.set_params(halofit_version='takahashi')
+    # get results, it must run before the power speactra calculation
+    results = camb.get_results(pars)
+
+    return results, pars
+
+
+def get_PK(H0, omg_cm0h2, omg_bm0h2, As, ns, zpts, kpts_num=300, kmin=1e-4, kmax=3):
+    # set parameters
+    pars = camb.CAMBparams()
+    pars.set_cosmology(H0=H0, ombh2=omg_bm0h2, omch2=omg_cm0h2)
+    pars.InitPower.set_params(As=As, ns=ns)
+    pars.set_matter_power(redshifts=zpts, kmax=kmax)
+
+    # Non-Linear spectra (Halofit)
+    pars.NonLinear = model.NonLinear_both
+    pars.NonLinearModel.set_params(halofit_version='takahashi')
+    # get results, it must run before the power speactra calculation
+    results = camb.get_results(pars)
+
     results.calc_power_spectra(pars)
-    kh_nonlin, z_nonlin, pk_nonlin = results.get_matter_power_spectrum(minkh=1e-4, maxkh=3, npoints=200)
+    kh_nonlin, z_nonlin, pk_nonlin = results.get_matter_power_spectrum(minkh=kmin, maxkh=kmax, npoints=kpts_num)
     s8 = numpy.array(results.get_sigma8())
+
+    return z_nonlin, kh_nonlin, pk_nonlin, s8
+
+
+def get_interpPk(cambPk, zpts, kpts):
+    znum, knum = zpts.shape[0], kpts.shape[0]
+    pk_result = numpy.zeros((znum, knum))
+    for i in range(znum):
+        pk_result[i] = cambPk.P(zpts[i], kpts)
+    return pk_result
+
+
+def get_PK_interp(H0, omg_cm0h2, omg_bm0h2, As, ns, zpts, kmax=5):
+    # set parameters
+    pars = camb.CAMBparams()
+    pars.set_cosmology(H0=H0, ombh2=omg_bm0h2, omch2=omg_cm0h2)
+    pars.InitPower.set_params(As=As, ns=ns)
+    # Non-Linear spectra (Halofit)
+    pars.NonLinearModel.set_params(halofit_version='takahashi')
+
+    PK = camb.get_matter_power_interpolator(pars, nonlinear=True, hubble_units=True, k_hunit=True, kmax=kmax, zs=zpts)
+
+    return PK
+
+
+def ready4PL(Lmin, Lmax, Lpts_num, kmin, kmax, com_dist, zpts_num):
+    # calculate the k corresponding to L (specified by user)
+    Lpts = tool_box.set_bin_log(Lmin, Lmax, Lpts_num)
+    integ_k = numpy.zeros((zpts_num, Lpts_num))
+
+    for i in range(Lpts_num):
+        # integrate PK for each fixed L
+        # k = L/com_dist at Z
+        integ_k[:, i] = Lpts[i] / com_dist
+
+    print("k h/Mpc ~[%.4f, %.4f]" % (integ_k.min(), integ_k.max()))
+
+    # only in the interval, [kmin, kmax], will be calculated
+    idx = integ_k < kmin
+    integ_k[idx] = 0
+    idx = integ_k > kmax
+    integ_k[idx] = 0
+
+    return Lpts, integ_k
+
+
+def get_PL(pk_integ, factor_integ, delta_com_dist):
+    Lpts_num = pk_integ.shape[1]
+    PL = numpy.zeros((Lpts_num,))
+
+    for i in range(Lpts_num):
+        integ_part = pk_integ[:, i] * factor_integ
+        PL[i] = numpy.sum((integ_part[1:] + integ_part[:-1]) / 2 * delta_com_dist)
+
+    return PL
--- CFHTLenS/Fourier_Quad/correlation_exposure_wise/plot_result.py
+++ CFHTLenS/Fourier_Quad/correlation_exposure_wise/plot_result.py
@@ -12,100 +12,107 @@ import matplotlib.pyplot as plt
 zbin_num = 6#int(argv[1])
 theta_bin_num = 5#int(argv[2])
 resample_num = 200#int(argv[3])
-pts_num = int(theta_bin_num*(zbin_num**2+zbin_num)/2)
+
+pts_num = int(theta_bin_num * (zbin_num ** 2 + zbin_num) / 2)
 data_path = "E:/works/correlation/CFHT/cut_2.5/smooth"
-pic_nm_p = data_path + "/chi_plus_result_%d_same_expo.png"%resample_num
-pic_nm_m = data_path + "/chi_minus_result_%d_same_expo.png"%resample_num
-result_npz = data_path + "/result_cache_%d.npz"%resample_num
-h5f = h5py.File(data_path + "/result_%d_same_expo.hdf5"%resample_num,"r")
-print(list(h5f.keys()))
-# print(list(h5f["/0"].keys()))
-
-
-xi_p = (-h5f["/%d/tt"%resample_num][()] - h5f["/%d/xx"%resample_num][()]).reshape((1,pts_num))
-xi_m = (-h5f["/%d/tt"%resample_num][()] + h5f["/%d/xx"%resample_num][()]).reshape((1,pts_num))
-theta = h5f["/%d/theta"%resample_num][()].reshape((1,pts_num))
-# print(theta.reshape((21,7)))
-
-
-xi_p_sig = numpy.zeros_like(xi_p)
-xi_m_sig = numpy.zeros_like(xi_p)
-
-# results of each jack
-xi_p_sub = numpy.zeros((resample_num, pts_num))
-xi_m_sub = numpy.zeros((resample_num, pts_num))
-
-for i in range(resample_num):
-
-    xi_p_sub[i] = (-h5f["/%d/tt"%i][()] - h5f["/%d/xx"%i][()]).reshape((1,pts_num))
-    xi_m_sub[i] = (-h5f["/%d/tt"%i][()] + h5f["/%d/xx"%i][()]).reshape((1,pts_num))
-
-for i in range(pts_num):
-    xi_p_sig[:,i] = xi_p_sub[:,i].std()*numpy.sqrt(resample_num-1)
-    xi_m_sig[:,i] = xi_m_sub[:,i].std()*numpy.sqrt(resample_num-1)
-
-numpy.savez(result_npz, theta, xi_p, xi_m, xi_p_sig, xi_m_sig)
 
 # chi_plus
-img = Image_Plot(fig_x=4, fig_y=3,xpad=0,ypad=0,axis_linewidth=2.5, plt_line_width=3, legend_size=25,xy_tick_size=25)
+img = Image_Plot(fig_x=4, fig_y=3,xpad=0,ypad=0,axis_linewidth=2.5, plt_line_width=3, legend_size=35,xy_tick_size=25)
 img.subplots(zbin_num, zbin_num)
 img.set_style()
+pic_nm_p = data_path + "/chi_plus_result_%d_compare.png" % resample_num
 
-for i in range(zbin_num):
-    for j in range(zbin_num-i,zbin_num):
+expo_type = ["diff_expo","same_expo"]
+for ii in range(2):
 
-        img.figure.delaxes(img.axs[i][j])
+    # pic_nm_p = data_path + "/chi_plus_result_%d_%s.png"%(resample_num,expo_type[ii])
+    # pic_nm_m = data_path + "/chi_minus_result_%d_%s.png"%(resample_num,expo_type[ii])
+    result_npz = data_path + "/result_cache_%d_%s.npz"%(resample_num,expo_type[ii])
+    h5f = h5py.File(data_path + "/result_%d_%s.hdf5"%(resample_num,expo_type[ii]),"r")
+    print(list(h5f.keys()))
+    # print(list(h5f["/0"].keys()))
 
-img.axis_type(0,"major",10)
-img.axis_type(1,"major",10)
-img.axis_type(0,"minor",5)
-img.axis_type(1,"minor",5)
 
-tag = 0
-for i in range(zbin_num):
-    for j in range(zbin_num):
-        img_row = zbin_num - j - 1
-        img_col = i
-        if j >= i:
-            st, ed = int(tag*theta_bin_num), int((tag+1)*theta_bin_num)
-            # print(theta[st:ed])
+    xi_p = (-h5f["/%d/tt"%resample_num][()] - h5f["/%d/xx"%resample_num][()]).reshape((1,pts_num))
+    xi_m = (-h5f["/%d/tt"%resample_num][()] + h5f["/%d/xx"%resample_num][()]).reshape((1,pts_num))
+    theta = h5f["/%d/theta"%resample_num][()].reshape((1,pts_num))
+    # print(theta.reshape((21,7)))
 
-            img.axs[img_row][img_col].errorbar(theta[0,st:ed], xi_p[0,st:ed],yerr=xi_p_sig[0,st:ed],
-                                               lw=img.plt_line_width,elinewidth=img.plt_line_width,
-                                               marker="o",fmt=" ",mfc="none",ms=12,label="$\\xi_{+}$",capsize=5)
-            if tag == 0:
-                img.axs[img_row][img_col].legend(loc="lower left",bbox_to_anchor=(1.2,0.25),
-                                                 fancybox=False,fontsize=img.legend_size)
 
-            img.axs_text(img_row, img_col, 0.8, 0.8, "%d-%d" % (i + 1, j + 1), text_fontsize=img.legend_size, text_color="k")
+    xi_p_sig = numpy.zeros_like(xi_p)
+    xi_m_sig = numpy.zeros_like(xi_p)
 
-            # img.axs[img_row][img_col].set_xticks(fontsize=img.legend_size)
-            # img.axs[img_row][img_col].set_yticks(fontsize=img.legend_size)
-            img.axs[img_row][img_col].set_yscale("log")
-            img.axs[img_row][img_col].set_xscale("log")
+    # results of each jack
+    xi_p_sub = numpy.zeros((resample_num, pts_num))
+    xi_m_sub = numpy.zeros((resample_num, pts_num))
 
-            img.axs[img_row][img_col].set_xlim(0.8, 60)
-            img.set_ticklabel_str(img_row, img_col, 1,[1,5,10,20,40], ["$1$","$5$","$10$","$20$","$40$"])
+    for i in range(resample_num):
 
-            # img.axs[img_row][img_col].set_xlim(0.8, 200)
-            # img.set_ticklabel_str(img_row, img_col, 1,[1,5,10,100], ["$1$","$5$","$10$","$100$"])
-            img.axs[img_row][img_col].set_ylim(5 * 10 ** (-7), 5 * 10 ** (-4))
+        xi_p_sub[i] = (-h5f["/%d/tt"%i][()] - h5f["/%d/xx"%i][()]).reshape((1,pts_num))
+        xi_m_sub[i] = (-h5f["/%d/tt"%i][()] + h5f["/%d/xx"%i][()]).reshape((1,pts_num))
 
-            # if tag not in [0, 6, 11, 15, 18, 20]:
-            #     # img.del_axis(img_row,img_col,[1])
-            #     img.axs[img_row][img_col].set_xticklabels([])
-            if img_col != 0:
-                img.axs[img_row][img_col].set_yticklabels([])
-                # img.axs[img_row][img_col].set_yticks([])
+    for i in range(pts_num):
+        xi_p_sig[:,i] = xi_p_sub[:,i].std()*numpy.sqrt(resample_num-1)
+        xi_m_sig[:,i] = xi_m_sub[:,i].std()*numpy.sqrt(resample_num-1)
 
-            tag += 1
+# numpy.savez(result_npz, theta, xi_p, xi_m, xi_p_sig, xi_m_sig)
+
+    if ii == 0:
+        for i in range(zbin_num):
+            for j in range(zbin_num-i,zbin_num):
+                img.figure.delaxes(img.axs[i][j])
+
+    img.axis_type(0,"major",10)
+    img.axis_type(1,"major",10)
+    img.axis_type(0,"minor",5)
+    img.axis_type(1,"minor",5)
+
+    tag = 0
+    for i in range(zbin_num):
+        for j in range(zbin_num):
+            img_row = zbin_num - j - 1
+            img_col = i
+            if j >= i:
+                st, ed = int(tag*theta_bin_num), int((tag+1)*theta_bin_num)
+                # print(theta[st:ed])
+
+                img.axs[img_row][img_col].errorbar(theta[0,st:ed]+theta[0,st:ed]*0.1*ii, xi_p[0,st:ed],yerr=xi_p_sig[0,st:ed],
+                                                   lw=img.plt_line_width, elinewidth=img.plt_line_width, c="C%d"%ii,
+                                                   marker="o",fmt=" ",mfc="none",ms=12,label="$\\xi_{+}$ %s"%expo_type[ii],capsize=5)
+                if tag == 0:
+                    img.axs[img_row][img_col].legend(loc="lower left",bbox_to_anchor=(4,1),
+                                                     fancybox=False,fontsize=img.legend_size)
+
+                img.axs_text(img_row, img_col, 0.8, 0.8, "%d-%d" % (i + 1, j + 1), text_fontsize=img.legend_size, text_color="k")
+
+                # img.axs[img_row][img_col].set_xticks(fontsize=img.legend_size)
+                # img.axs[img_row][img_col].set_yticks(fontsize=img.legend_size)
+                img.axs[img_row][img_col].set_yscale("log")
+                img.axs[img_row][img_col].set_xscale("log")
+
+                img.axs[img_row][img_col].set_xlim(0.8, 60)
+                img.set_ticklabel_str(img_row, img_col, 1,[1,5,10,20,40], ["$1$","$5$","$10$","$20$","$40$"])
+
+                # img.axs[img_row][img_col].set_xlim(0.8, 200)
+                # img.set_ticklabel_str(img_row, img_col, 1,[1,5,10,100], ["$1$","$5$","$10$","$100$"])
+                img.axs[img_row][img_col].set_ylim(4 * 10 ** (-7), 7 * 10 ** (-4))
+
+                # if tag not in [0, 6, 11, 15, 18, 20]:
+                #     # img.del_axis(img_row,img_col,[1])
+                #     img.axs[img_row][img_col].set_xticklabels([])
+                if img_col != 0:
+                    img.axs[img_row][img_col].set_yticklabels([])
+                    # img.axs[img_row][img_col].set_yticks([])
+
+                tag += 1
 
 
 
 img.save_img(pic_nm_p)
 img.close_img()
 # img.show_img()
-# chi_plus
+exit()
+# chi_minus
 img = Image_Plot(fig_x=4, fig_y=3,xpad=0,ypad=0,axis_linewidth=2.5, plt_line_width=3, legend_size=25,xy_tick_size=25)
 img.subplots(zbin_num, zbin_num)
 img.set_style()
