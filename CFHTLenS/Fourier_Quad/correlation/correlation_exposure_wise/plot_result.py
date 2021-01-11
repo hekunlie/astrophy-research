78c8a9a93d1b9d358c64e2908fdfc0b6c96705b1
hekunlie <hekun_lee@sjtu.edu.cn>
2021-01-11 19:43:03 +0800

    Update plot_result.py

--- CFHTLenS/Fourier_Quad/correlation/correlation_exposure_wise/plot_result.py
+++ CFHTLenS/Fourier_Quad/correlation/correlation_exposure_wise/plot_result.py
@@ -18,16 +18,14 @@ pts_num = int(theta_bin_num * (zbin_num ** 2 + zbin_num) / 2)
 data_path = "E:/works/correlation/CFHT/cut_2.5/smooth"
 
 pk_lines_tag = 0
-if os.path.exists(data_path + "/tomo_pk.npz"):
-    npz = numpy.load(data_path + "/tomo_pk.npz")
-    xi_p_theoretical_lines = npz["arr_6"]
-    xi_m_theoretical_lines = npz["arr_7"]
-    xi_theta = npz["arr_5"]
+if os.path.exists(data_path + "/theoretical_pk.npz"):
+    npz = numpy.load(data_path + "/theoretical_pk.npz")
+    xi_p_theoretical_lines = npz["arr_0"]
+    xi_m_theoretical_lines = npz["arr_1"]
+    xi_theta = npz["arr_2"]
 
     pk_lines_tag = 1
     print("Find Pk lines")
-
-
 
 expo_type = ["diff_expo","same_expo"]
 
@@ -137,7 +135,7 @@ for ii in range(2):
 
                 if ii == 0 and pk_lines_tag == 1:
                     pk_line_label = "Planck2018:\n$\Omega_m=0.313$\n$\sigma_8=0.8097$"
-                    img.axs[img_row][img_col].plot(xi_theta, xi_p_theoretical_lines[tag], c="k",ls="dashdot", label=pk_line_label)
+                    img.axs[img_row][img_col].plot(xi_theta[tag], xi_p_theoretical_lines[tag], c="k",ls="dashdot", label=pk_line_label)
 
                 img.axs[img_row][img_col].set_yscale("log")
                 img.axs[img_row][img_col].set_xscale("log")
@@ -200,7 +198,7 @@ for ii in range(2):
 
                 if ii == 0 and pk_lines_tag == 1:
                     pk_line_label = "Planck2018:\n$\Omega_m=0.313$\n$\sigma_8=0.8097$"
-                    img.axs[img_row][img_col].plot(xi_theta, xi_m_theoretical_lines[tag], c="k",ls="dashdot", label=pk_line_label)
+                    img.axs[img_row][img_col].plot(xi_theta[tag], xi_m_theoretical_lines[tag], c="k",ls="dashdot", label=pk_line_label)
 
                 img.axs[img_row][img_col].set_yscale("log")
                 img.axs[img_row][img_col].set_xscale("log")
@@ -208,76 +206,15 @@ for ii in range(2):
                 img.axs[img_row][img_col].set_xlim(0.8, 60)
                 img.set_ticklabel_str(img_row, img_col, 1,[1,5,10,20,40], ["$1$","$5$","$10$","$20$","$40$"])
 
-                # img.axs[img_row][img_col].set_ylim(4 * 10 ** (-8), 7 * 10 ** (-5))
+                img.axs[img_row][img_col].set_ylim(4 * 10 ** (-8), 7 * 10 ** (-5))
 
                 if img_col != 0:
                     img.axs[img_row][img_col].set_yticklabels([])
 
                 tag += 1
 
-
-
 img.save_img(pic_nm)
 img.close_img()
 print(pic_nm)
 
 
-exit()
-
-# chi_minus
-img = Image_Plot(fig_x=4, fig_y=3,xpad=0,ypad=0,axis_linewidth=2.5, plt_line_width=3, legend_size=25,xy_tick_size=25)
-img.subplots(zbin_num, zbin_num)
-img.set_style()
-
-for i in range(zbin_num):
-    for j in range(zbin_num-i,zbin_num):
-
-        img.figure.delaxes(img.axs[i][j])
-
-img.axis_type(0,"major",10)
-img.axis_type(1,"major",10)
-img.axis_type(0,"minor",5)
-img.axis_type(1,"minor",5)
-
-tag = 0
-for i in range(zbin_num):
-    for j in range(zbin_num):
-        img_row = zbin_num - j - 1
-        img_col = i
-        if j >= i:
-            st, ed = int(tag*theta_bin_num), int((tag+1)*theta_bin_num)
-            # print(theta[st:ed])
-            img.axs[img_row][img_col].errorbar(theta[0,st:ed], xi_m[0,st:ed],yerr=xi_m_sig[0,st:ed],
-                                               lw=img.plt_line_width,elinewidth=img.plt_line_width,
-                                               marker="o",fmt=" ",color="C1",mfc="none",ms=12,label="$\\xi_{-}$",capsize=5)
-            if tag == 0:
-                img.axs[img_row][img_col].legend(loc="lower left",bbox_to_anchor=(1.2,0.25),
-                                                 fancybox=False,fontsize=img.legend_size)
-
-            img.axs_text(img_row, img_col, 0.8, 0.8, "%d-%d" % (i + 1, j + 1), text_fontsize=img.legend_size, text_color="k")
-
-            # img.axs[img_row][img_col].set_xticks(fontsize=img.legend_size)
-            # img.axs[img_row][img_col].set_yticks(fontsize=img.legend_size)
-            img.axs[img_row][img_col].set_yscale("log")
-            img.axs[img_row][img_col].set_xscale("log")
-
-            img.axs[img_row][img_col].set_xlim(0.8, 60)
-            img.set_ticklabel_str(img_row, img_col, 1,[1,5,10,20,40], ["$1$","$5$","$10$","$20$","$40$"])
-
-            # img.axs[img_row][img_col].set_xlim(0.8, 200)
-            # img.set_ticklabel_str(img_row, img_col, 1,[1,5,10,100], ["$1$","$5$","$10$","$100$"])
-            img.axs[img_row][img_col].set_ylim(5 * 10 ** (-7), 5 * 10 ** (-4))
-
-            # if tag not in [0, 6, 11, 15, 18, 20]:
-            #     # img.del_axis(img_row,img_col,[1])
-            #     img.axs[img_row][img_col].set_xticklabels([])
-            if img_col != 0:
-                img.axs[img_row][img_col].set_yticklabels([])
-                # img.axs[img_row][img_col].set_yticks([])
-
-            tag += 1
-
-
-
-img.save_img(pic_nm_m)
-img.close_img()
