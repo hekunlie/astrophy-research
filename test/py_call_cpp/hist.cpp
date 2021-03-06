b6a9a766059183e81a62972a1b814a1b49e28e1b
hekunlie <hekun_lee@sjtu.edu.cn>
2021-03-06 22:02:12 +0800

    update

---
+++ test/py_call_cpp/hist.cpp
@@ -0,0 +1,160 @@
+#include<ctime>
+#include<iostream>
+extern "C"
+{
+    void test1(double x, double y)
+    {
+        double c = x+y;
+        // std::cout<<c<<std::endl;
+    }
+
+    // this one doesn't work, "segmentation fault"
+    void test2(double x, double y, double &z)
+    {
+        z = x+y;
+        // std::cout<<z<<std::endl;
+    }
+    
+    double test3(double x, double y)
+    {
+        return x+y;
+    }
+
+    // 1-D array
+    double test4(int a, double *data)
+    {
+        double b = 0;
+        int i;
+        for(i=0;i<a;i++) b+=data[i];
+        return b;
+    }
+
+    // double test5(int a, double *data, int b, double *data2)
+    // {
+    //     double c = 0;
+    //     int i;
+    //     for(i=0;i<a;i++) c+=data[i];
+    //     std::cout<<"Count "<<c<<std::endl;
+    //     for(i=0;i<b;i++)
+    //     {
+    //         data2[i] = c;
+    //         std::cout<<i<<" "<<c<<std::endl;
+    //     }
+    // }
+
+
+    double test(double *data1, int len1, double *data2, int len2)
+    {
+        double accum = 0;
+        int i;
+        for(i=0; i<len1; i++) accum+=data1[i];
+
+        for(i=0; i<len2; i++)data2[i] = accum;
+    }
+
+
+    int locate(double x, double *bins, int bin_num)
+    {
+        int i,j,st,ed,mid;
+        st = 0;
+        ed = bin_num;
+ 
+        while(true)
+        {   
+            if(ed - st <= 1){break;}
+
+            mid = (st+ed)/2;
+            if(x >= bins[mid]){st = mid;}
+            else{ed = mid;}
+        }
+        return st;
+    }
+    
+    void hist2d(double *x, double *y, int num, double *xbin, double *ybin, int xbin_num, int ybin_num, int *count)//, double *grid_x, double *grid_y)
+    {   
+        int i, j, k;
+        int xmid, ymid;
+        
+        int xtag, ytag;
+        double t1, t2;
+
+        t1 = clock();
+        for(i=0; i<num; i++)
+        {
+            for(j=0;j<xbin_num;j++)
+            {
+                if(x[i]>=xbin[j] && x[i]<xbin[j+1])
+                {
+                    xtag = j;break;
+                }
+            }
+
+            for(j=0;j<ybin_num;j++)
+            {
+                if(y[i]>=ybin[j] && y[i]<ybin[j+1])
+                {
+                    ytag = j;break;
+                }
+            }
+
+            count[ytag*xbin_num + xtag] += 1;
+            // grid_x[ytag*xbin_num + xtag] += x[i];
+            // grid_y[ytag*xbin_num + xtag] += y[i];
+        }
+        // for(i=0;i=xbin_num*ybin_num;i++)
+        // {
+        //     grid_x[i] = grid_x[i]/count[i];
+        //     grid_y[i] = grid_y[i]/count[i];
+        // }
+        t2 = clock();
+        std::cout<<(t2-t1)/CLOCKS_PER_SEC<<std::endl;
+    }
+
+    void hist2d_fast(double *x, double *y, int num, double *xbin, double *ybin, int xbin_num, int ybin_num, int *count, double *grid_x, double *grid_y)
+    {   
+        int i, j, k;
+
+        int xst, xed, xmid;
+        int yst, yed, ymid;
+
+        // double t1, t2;
+
+        // t1 = clock();
+        for(i=0; i<num; i++)
+        {
+
+            xst = 0;
+            xed = xbin_num;
+            while(xed - xst>1)
+            {   
+                xmid = (xst+xed)/2;
+                if(x[i] >= xbin[xmid]){xst = xmid;}
+                else{xed = xmid;}
+            }
+ 
+            
+            yst = 0;
+            yed = ybin_num;
+            while(yed - yst > 1)
+            {   
+                ymid = (yst+yed)/2;
+                if(y[i] >= ybin[ymid]){yst = ymid;}
+                else{yed = ymid;}
+            }
+
+            
+            k = yst*xbin_num + xst;
+            count[k] += 1;
+            grid_x[k] += x[i];
+            grid_y[k] += y[i];
+        }
+        for(i=0;i<xbin_num*ybin_num;i++)
+        {
+            grid_x[i] = grid_x[i]/count[i];
+            grid_y[i] = grid_y[i]/count[i];
+        }
+        // t2 = clock();
+        // std::cout<<(t2-t1)/CLOCKS_PER_SEC<<std::endl;
+    }
+
+}
--- test/py_call_cpp/py_call_cpp.py
+++ test/py_call_cpp/py_call_cpp.py
@@ -2,11 +2,76 @@ import numpy
 import ctypes
 import numpy.ctypeslib as ctl
 from numpy.ctypeslib import ndpointer
+import time
 
 
 lib = ctypes.cdll.LoadLibrary("libhist.so")
 
-hist = lib.hist2d
+
+# test1 = lib.test1
+# test1.restype = None
+# test1.argtypes = [ctypes.c_double, ctypes.c_double]
+# a,b = 10,15
+# test1(a,b)
+
+# this doesn't work, "segmentation fault"
+# test2 = lib.test2
+# test2.restype = None
+# test2.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double]
+# a,b = 10,15
+# c = 1
+# test2(a,b,c)
+# print(c)
+
+
+
+# test3 = lib.test3
+# test3.restype = ctypes.c_double
+# test3.argtypes = [ctypes.c_double, ctypes.c_double]
+# a,b = 10,15
+# c = test3(a, b)
+# print(c)
+#
+#
+# test4 = lib.test4
+# test4.restype = ctypes.c_double
+# test4.argtypes = [ctypes.c_int, ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous')]
+# a = 1000
+# b = numpy.ones((a, ), dtype=numpy.float64)
+# c = test4(a, b)
+# print(c)
+
+
+# test = lib.test
+# test.restype = None
+# test.argtypes = [ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous'),ctypes.c_int,
+#                  ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous'), ctypes.c_int]
+#
+# num1, num2 = 1000, 10
+# sum_val = ctypes.byref(ctypes.c_double())
+# arr1 = numpy.ones((num1, ), dtype=numpy.float64)
+# arr2 = numpy.zeros((num2, ), dtype=numpy.float64)
+#
+# test(arr1, num1, arr2, num2)
+#
+# print(arr2)
+
+
+
+# locate = lib.locate
+# locate.restype = ctypes.c_int
+# locate.argtypes = [ctypes.c_double, ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous'), ctypes.c_int]
+#
+# bin_num = 100
+# a = numpy.random.uniform(-1000,1000,1)[0]
+# bins = numpy.linspace(-1000,1000,bin_num+1)
+#
+# bin_tag = locate(a, bins, bin_num)
+# print(bin_tag, bins[bin_tag],a, bins[bin_tag+1])
+
+
+
+hist = lib.hist2d_fast
 # hist_fast = lib.hist_fast
 
 hist.restype = None
@@ -17,28 +82,35 @@ hist.argtypes = [ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous'),
                  ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous'),
                  ctypes.c_int,
                  ctypes.c_int,
-                 ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous'),
+                 ctl.ndpointer(numpy.intc, flags='aligned, c_contiguous'),
                  ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous'),
                  ctl.ndpointer(numpy.float64, flags='aligned, c_contiguous')]
 
-xbin_num = 1000
-ybin_num = 800
-data_num = 30000
+xbin_num = 500
+ybin_num = 500
+data_num = 3000000
 
 x = numpy.random.uniform(-1000, 1000, data_num).astype(numpy.float64)
 y = numpy.random.uniform(-1000, 1000, data_num).astype(numpy.float64)
 
 xbins = numpy.linspace(-1000,1000,xbin_num+1, dtype=numpy.float64)
 ybins = numpy.linspace(-1000,1000,ybin_num+1, dtype=numpy.float64)
-count = numpy.zeros((ybin_num, xbin_num), dtype=numpy.float64)
+count = numpy.zeros((ybin_num, xbin_num), dtype=numpy.intc)
 grid_x = numpy.zeros_like(count, dtype=numpy.float64)
 grid_y = numpy.zeros_like(count, dtype=numpy.float64)
 
-hist(x, y, data_num, xbins, ybins, xbin_num, ybin_num, count, grid_x, grid_y)
+t1 = time.time()
 
+hist(x, y, data_num, xbins, ybins, xbin_num, ybin_num, count, grid_x, grid_y)
+t2 = time.time()
 num2d = numpy.histogram2d(y, x, [ybins, xbins])[0]
+t3 = time.time()
 diff = num2d - count
+numpy.savez("./cache.npz", count, num2d, diff,grid_x,grid_y)
 print(diff.min(), diff.max())
+print(t2-t1, " sec")
+print(t3-t2, " sec")
+
 
 
 
