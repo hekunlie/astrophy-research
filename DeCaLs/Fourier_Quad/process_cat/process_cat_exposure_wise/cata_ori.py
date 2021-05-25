ff97d9120a64ded76636ea9750542425611661f2
Star_Burst <hekun_lee@sjtu.edu.cn>
2021-05-25 21:59:08 +0800

    Update cata_ori.py

--- DeCaLs/Fourier_Quad/process_cat/process_cat_exposure_wise/cata_ori.py
+++ DeCaLs/Fourier_Quad/process_cat/process_cat_exposure_wise/cata_ori.py
@@ -17,27 +17,27 @@ rank = comm.Get_rank()
 numprocs = comm.Get_size()
 
 
-ori_cat_chara = "_all_alt.cat"
+ori_cat_chara = "_all.cat"
 
 data_band = ["r", "z"]
 
 total_path = "/lustre/home/acct-phyzj/phyzj-sirius/hklee/work/DECALS/cat_hdf5_ori"
 ori_cat_path = "/lustre/home/acct-phyzj/share/DECALS_all_with_flat"
 
-if rank == 0:
-    for ib in data_band:
-        files = os.listdir(ori_cat_path + "/" + ib)
-
-        band_files = []
-
-        for fnm in files:
-            if ori_cat_chara in fnm:
-                band_files.append("%s\n"%fnm.split(".")[0])
-
-        with open(total_path + "/code/exposures_%s_band.dat"%ib,"w") as f:
-            f.writelines(band_files)
-
-comm.Barrier()
+# if rank == 0:
+#     for ib in data_band:
+#         files = os.listdir(ori_cat_path + "/" + ib)
+#
+#         band_files = []
+#
+#         for fnm in files:
+#             if ori_cat_chara in fnm:
+#                 band_files.append("%s\n"%fnm.split(".")[0])
+#
+#         with open(total_path + "/code/exposures_%s_band.dat"%ib,"w") as f:
+#             f.writelines(band_files)
+#
+# comm.Barrier()
 
 need_idx = [0,1, 5,6, 14,21, 24,25, 26,27,28,29,30]
 for ib in data_band:
@@ -50,7 +50,7 @@ for ib in data_band:
 
     for fnm in files:
         nm = fnm.split("\n")[0]
-        src_path = ori_cat_path + "/%s/%s.dat"%(ib, nm)
+        src_path = ori_cat_path + "/%s/%s.cat"%(ib, nm)
         dst_path = total_path + "/%s/%s.hdf5"%(ib, nm)
 
         try:
@@ -60,7 +60,11 @@ for ib in data_band:
             if os.path.exists(src_path):
                 log_inform = "%d Failed in reading %s %d Bytes !\n" % (
                     rank, src_path, os.path.getsize(src_path))
+                print(log_inform)
             row, col = 0, 0
+        log_inform = "%d Failed in reading %s %d Bytes !\n" % (
+            rank, src_path, os.path.getsize(src_path))
+        print(log_inform)
 
         if row > 0:
 
