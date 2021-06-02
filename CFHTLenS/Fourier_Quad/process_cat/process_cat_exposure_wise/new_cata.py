96a5ba0240f15e88a0c0dca87662e5ab785d2ef2
hekunlie <hekun_lee@sjtu.edu.cn>
2021-06-02 23:35:27 +0800

    Update new_cata.py

--- CFHTLenS/Fourier_Quad/process_cat/process_cat_exposure_wise/new_cata.py
+++ CFHTLenS/Fourier_Quad/process_cat/process_cat_exposure_wise/new_cata.py
@@ -19,25 +19,35 @@ for i, ii in enumerate(headers):
         head_list.append(ii)
 print(len(head_list))
 headers = "\t".join(head_list)
-print(headers)
 
 pzs = []
 paras = [headers]
-for i in range(1,len(cc)):
-#     print(cc[i])
+print(paras)
+
+for i in range(1, len(cc)):
+
     ic = cc[i].split("\n")[0].split(",\"")
-    para_ = ic[0].split(",")[1:]
-#     print(para_)
+
+    all_para_ = ic[0].split(",")
+    field_id = all_para_[0]
+    para_ = all_para_[1:]
+
     pz_, mag = ic[1].split("\"")
     pz_ = pz_.split(",")
-    para_.append(mag.split(",")[1])
-#     print(pz_, mag)
-#     print(len(pz_))
-#     print(para_,len(para_))
-    paras.append("\t".join(para_) + "\n")
-    pzs.append("\t".join(pz_)+ "\n")
-# print(paras)
-# print(pzs)
+    pzs.append("\t".join(pz_) + "\n")
+
+    all_para_[0] = all_para_[0].replace("W", "1")
+    if "p" in all_para_[0]:
+        all_para_[0] = all_para_[0].replace("p", "2")
+    if "m" in all_para_[0]:
+        all_para_[0] = all_para_[0].replace("m", "3")
+    all_para_[0] = all_para_[0].replace("_", "")
+
+    sub_paras = []
+    sub_paras.extend(all_para_)
+    sub_paras.append(mag.split(",")[1])
+    paras.append("\t".join(sub_paras) + "\n")
+    # print(sub_paras, len(sub_paras), "\n\n")
 
 with open("/home/hklee/work/CFHT/CFHTLens.cat","w") as f:
     f.writelines(paras)
