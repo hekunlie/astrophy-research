a1d77079e31e1b0d9a2732e07dd88cd5d91cd9d1
hekunlie <hekun_lee@sjtu.edu.cn>
2021-06-03 00:03:17 +0800

    Update new_cata.py

--- CFHTLenS/Fourier_Quad/process_cat/process_cat_exposure_wise/new_cata.py
+++ CFHTLenS/Fourier_Quad/process_cat/process_cat_exposure_wise/new_cata.py
@@ -7,22 +7,29 @@ import h5py
 import time
 import os
 
+zbin = numpy.array([0.025 + i * 0.05 for i in range(70)])
 
 with open("/home/hklee/work/CFHT/CFHTLens.tsv","r") as f:
     cc = f.readlines()
 
-headers = cc[0].split(",")
+src_num = len(cc) - 1
+
+headers = cc[0].split("\n")[0].split(",")
 print(headers)
 head_list = ["#"]
 for i, ii in enumerate(headers):
     if ii != "PZ_full":
         head_list.append(ii)
+head_list.append("Z_E\n")
+
 print(len(head_list))
 headers = "\t".join(head_list)
 
 pzs = []
 paras = [headers]
 print(paras)
+
+pz_arr = numpy.zeros((src_num, 70),dtype=numpy.float32)
 
 for i in range(1, len(cc)):
 
@@ -34,7 +41,14 @@ for i in range(1, len(cc)):
 
     pz_, mag = ic[1].split("\"")
     pz_ = pz_.split(",")
-    pzs.append("\t".join(pz_) + "\n")
+    for ii in range(len(pz_)):
+        pz_arr[i-1, ii] = float(pz_[ii])
+
+    pz_sum = numpy.sum(pz_arr[i-1])
+    if pz_sum != 0:
+        ze = numpy.sum(zbin*pz_arr[i-1])/pz_sum
+    else:
+        ze = -99.0
 
     all_para_[0] = all_para_[0].replace("W", "1")
     if "p" in all_para_[0]:
@@ -46,10 +60,14 @@ for i in range(1, len(cc)):
     sub_paras = []
     sub_paras.extend(all_para_)
     sub_paras.append(mag.split(",")[1])
+    sub_paras.append("%f"%ze)
     paras.append("\t".join(sub_paras) + "\n")
     # print(sub_paras, len(sub_paras), "\n\n")
 
 with open("/home/hklee/work/CFHT/CFHTLens.cat","w") as f:
     f.writelines(paras)
-with open("/home/hklee/work/CFHT/CFHTLens_pz.cat","w") as f:
-    f.writelines(pzs)
+
+numpy.savez("/home/hklee/work/CFHT/CFHTLens_pz.npz", pz_arr)
+
+
+
